5febc881d0ccfcca1f1e78b2f7a6df906ca089ba
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-11 18:14:35 +0100

    feat(meili search): add seeding

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,5 +1,6 @@
 import { backgroundsSeed } from "@/features/backgrounds/seed/seed";
 import { seedProjects } from "@/features/projects/seed";
+import { seedMeili } from "@/lib/meili-search";
 import { seedIdentities } from "@/features";
 import { seedDeveloperProfiles } from "@/features";
 
@@ -11,5 +12,6 @@ import { seedDeveloperProfiles } from "@/features";
     backgroundsSeed(),
   ]);
   await seedProjects();
+  await seedMeili();
   console.log("Done seeding...");
 })();
