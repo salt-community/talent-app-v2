40d0316e3c54a0ace83c1dba772339f6f7819354
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-11 16:15:35 +0100

    add new statusMenu component with dropdown

---
+++ src/features/admin/components/status-menu.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisVertical } from "lucide-react";
+import { Status } from "./status";
+import { Button } from "@/components";
+
+export function StatusMenu() {
+  const [status, setStatus] = React.useState("gray");
+
+  return (
+    <div className="flex items-center gap-4">
+      <Status status={status} />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <EllipsisVertical className="text-neutral fill-neutral" size={28} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuRadioGroup value={status} onValueChange={setStatus}>
+            <DropdownMenuRadioItem value="purple">
+              Highlighted
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="blue">
+              Published
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="gray">
+              Unpublished
+            </DropdownMenuRadioItem>
+            <Button size={"sm"} className="w-full">Delete</Button>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
