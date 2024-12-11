58f115de25c03c1acc9b51de68637ee9f848372b
CarlUdo <carl@wbsweden.com>
2024-12-11 15:52:39 +0100

    Make-new-function-to-calculate-category-score
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/logic.ts
+++ src/features/scores/logic.ts
@@ -25,4 +25,12 @@ export const calculateAverageAssignmentsScore = (assignments: Assignment[]) => {
   const scores = assignments.map(assignment => assignment.score)
   const averageAssignmentScore = scores.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / scores.length
   return Math.round(averageAssignmentScore);
+};
+
+export const calculateAverageCategoryScore = (assignments: Assignment[], category: string) => {
+  const scores = assignments
+    .filter(assignment => assignment.tags.includes(category)) 
+    .map(assignment => assignment.score);
+  const averageAssignmentScore = scores.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / scores.length
+  return Math.round(averageAssignmentScore);
 }
