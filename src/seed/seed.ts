d1682e1dff16be2ae065409a34bd23477d5726b1
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-11 18:14:35 +0100

    feat(meili search): add seeding

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,17 +1,19 @@
 import { backgroundsSeed } from "@/features/backgrounds/seed/seed";
 import { seedAssignments, seedScores } from "@/features";
 import { seedProjects } from "@/features/projects/seed";
+import { seedMeili } from "@/lib/meili-search";
 
 (async () => {
   console.log("Start seeding...");
 
   try {
-      await seedScores();
-      await backgroundsSeed();
-      await seedAssignments();
-      await seedProjects();
-      console.log("Done seeding");
+    await seedScores();
+    await backgroundsSeed();
+    await seedAssignments();
+    await seedProjects();
+    await seedMeili();
+    console.log("Done seeding");
   } catch (error) {
-      console.error("Error during seeding:", error);
+    console.error("Error during seeding:", error);
   }
 })();
