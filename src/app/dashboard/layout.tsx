8fe514a78d67b70c2121b26cacd1d525ba40daef
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-11 14:36:15 +0100

    comment out sidebar
    Co-authored-by: Carl <carl@wbsweden.com>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { DashboardSidebar } from "../../features/admin/components/dashboard-sidebar";
+// import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+// import { DashboardSidebar } from "../../features/admin/components/dashboard-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <DashboardSidebar />
+    // <SidebarProvider>
+    //   <DashboardSidebar />
       <main className="w-full">
-        <SidebarTrigger />
+        {/* <SidebarTrigger /> */}
         {children}
       </main>
-    </SidebarProvider>
+    // </SidebarProvider>
   );
 }
