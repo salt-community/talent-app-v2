424e68f5c2fc9be0163272bb757b0e2d2dcbeaa2
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-12 15:33:49 +0100

    add-cant-save-if-missing-title
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/components/add-assignment.tsx
+++ src/features/scores/components/add-assignment.tsx
@@ -1,4 +1,6 @@
-import { Pencil } from "lucide-react";
+"use client";
+
+import { ClipboardPlus } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -10,16 +12,22 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { addAssigmentAction } from "../actions";
+import { useState } from 'react';
 
 
 export function AddAssignment() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [title, setTitle] = useState("");
+
+  
   return (
-    <Dialog>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogTrigger asChild>
-        <Pencil
+        <ClipboardPlus
           type="submit"
           size={16}
           className="cursor-pointer hover:text-gray-600"
+          onClick={() => setIsDialogOpen(true)}
         />
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px] bg-white border border-gray-300 rounded-md shadow-md">
@@ -44,6 +52,8 @@ export function AddAssignment() {
                 type="text"
                 name="title"
                 className="col-span-3 border border-gray-300 rounded-md p-2 text-sm focus:ring focus:ring-gray-200 focus:outline-none"
+                onChange={(e) => (setTitle(e.target.value))}
+                required
               />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
@@ -57,8 +67,9 @@ export function AddAssignment() {
                 type="number"
                 min="0"
                 max="100"
-                name="teamCollaboration"
+                name="score"
                 className="col-span-3 border border-gray-300 rounded-md p-2 text-sm focus:ring focus:ring-gray-200 focus:outline-none"
+                required
               />
             </div>
             <div className="grid grid-cols-4 items-start gap-4">
@@ -180,6 +191,8 @@ export function AddAssignment() {
             <Button
               type="submit"
               className="bg-gray-800 text-white hover:bg-gray-700 text-sm px-4 py-2 rounded-md"
+              onClick={() => setIsDialogOpen(false)}
+              disabled={title.length === 0}
             >
               Save Changes
             </Button>
