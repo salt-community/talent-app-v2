a0312dc18eb2d1ee055a16a12c3cc1df504082bf
CarlUdo <carl@wbsweden.com>
2024-12-12 11:13:53 +0100

    Remove-unused-table
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/schema.ts
+++ src/features/scores/schema.ts
@@ -1,17 +1,5 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-
-
-export const scoresTable = pgTable("scores", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  frontend: integer().notNull(),
-  backend: integer().notNull(),
-  individualCommunication: integer().notNull(),
-  teamCollaboration: integer().notNull(),
-  design: integer().notNull(),
-  management: integer().notNull(),
-});
-
 export const assignmentTable = pgTable("score_assignments",{
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   userId: integer().notNull(),
