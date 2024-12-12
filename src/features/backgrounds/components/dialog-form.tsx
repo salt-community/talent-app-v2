6b8ff445f4f6c1a94e063727c9624c4a07476474
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-12 11:50:29 +0100

    feat(backgorund/form): make form scrollable with shadcn/ui ScrollArea component
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/backgrounds/components/dialog-form.tsx
+++ src/features/backgrounds/components/dialog-form.tsx
@@ -14,6 +14,7 @@ import {
 import { BackgroundSelect } from "../schema";
 import { TagsInput } from "./tags-input";
 import { Input, Label } from "@/components";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type Props = { background: BackgroundSelect };
 export function DialogForm({ background }: Props) {
@@ -22,76 +23,78 @@ export function DialogForm({ background }: Props) {
       <DialogTrigger asChild>
         <Pencil type="submit" size={16} />
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you´re done.
-          </DialogDescription>
-        </DialogHeader>
-        <form action={updateBackgroundAction}>
-          <div className="space-y-2 py-4">
-            <input
-              type="text"
-              name={"userId"}
-              defaultValue={background.id}
-              hidden
-            />
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              name="name"
-              id="name"
-              defaultValue={background.name}
-              placeholder="Fullname..."
-              className="col-span-3"
-            />
+      <DialogContent className="w-screen">
+        <ScrollArea className="h-[600px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you´re done.
+            </DialogDescription>
+          </DialogHeader>
+          <form action={updateBackgroundAction} className="p-2">
+            <div className="space-y-2 py-4">
+              <input
+                type="text"
+                name={"userId"}
+                defaultValue={background.id}
+                hidden
+              />
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                name="name"
+                id="name"
+                defaultValue={background.name}
+                placeholder="Fullname..."
+                className="col-span-3"
+              />
 
-            <Label htmlFor="title" className="text-right">
-              Role
-            </Label>
-            <Input
-              placeholder={background.title}
-              name="title"
-              id="title"
-              defaultValue="Role..."
-              className="col-span-3"
-            />
-            <Label htmlFor="bio" className="text-right">
-              Headline
-            </Label>
-            <Input
-              defaultValue={background.bio}
-              placeholder="Headline..."
-              name="bio"
-              id="bio"
-              className="col-span-3"
-            />
+              <Label htmlFor="title" className="text-right">
+                Role
+              </Label>
+              <Input
+                placeholder={background.title}
+                name="title"
+                id="title"
+                defaultValue="Role..."
+                className="col-span-3"
+              />
+              <Label htmlFor="bio" className="text-right">
+                Headline
+              </Label>
+              <Input
+                defaultValue={background.bio}
+                placeholder="Headline..."
+                name="bio"
+                id="bio"
+                className="col-span-3"
+              />
 
-            <TagsInput
-              tags={background.languages}
-              inputName={"languages"}
-              inputPlaceholder={"Add a language"}
-            />
+              <TagsInput
+                tags={background.languages}
+                inputName={"languages"}
+                inputPlaceholder={"Add a language"}
+              />
 
-            <TagsInput
-              tags={background.educations}
-              inputName={"educations"}
-              inputPlaceholder={"Add an education"}
-            />
+              <TagsInput
+                tags={background.educations}
+                inputName={"educations"}
+                inputPlaceholder={"Add an education"}
+              />
 
-            <TagsInput
-              tags={background.skills}
-              inputName={"skills"}
-              inputPlaceholder={"Add a skill"}
-            />
-          </div>
+              <TagsInput
+                tags={background.skills}
+                inputName={"skills"}
+                inputPlaceholder={"Add a skill"}
+              />
+            </div>
 
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </form>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
