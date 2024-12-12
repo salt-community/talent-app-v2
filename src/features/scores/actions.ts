feb8f5a97cae7b74f47976eb392ddfcc472a9294
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-12 15:10:33 +0100

    add-assignment-actios
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/actions.ts
+++ src/features/scores/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 import { scoresService } from "./instance";
+import { AssignmentUpdates } from "./types";
 
 export async function addAssigmentAction(formData: FormData) {
   const title = formData.get("title") as string;
@@ -9,31 +10,75 @@ export async function addAssigmentAction(formData: FormData) {
   const individualCommunication = formData.get(
     "individualCommunication"
   ) as string;
-  if(individualCommunication) tags.push("individualCommunication")
+  if (individualCommunication) tags.push("individualCommunication")
 
   const teamCollaboration = formData.get("teamCollaboration") as string;
-  if(teamCollaboration) tags.push("teamCollaboration")
+  if (teamCollaboration) tags.push("teamCollaboration")
 
   const management = formData.get("management") as string;
-  if(management) tags.push("management")
+  if (management) tags.push("management")
 
   const design = formData.get("design") as string;
-  if(design) tags.push("design")
+  if (design) tags.push("design")
 
   const backend = formData.get("backend") as string;
-  if(backend) tags.push("backend")
+  if (backend) tags.push("backend")
 
   const frontend = formData.get("frontend") as string;
-  if(frontend) tags.push("frontend")
+  if (frontend) tags.push("frontend")
 
 
   const newAssignment = {
     userId: 1,
     comment,
     score: Number(score),
-    title, 
+    title,
     tags,
   };
 
-  scoresService.addAssignment( newAssignment );
+  scoresService.addAssignment(newAssignment);
+}
+
+export async function editAssigmentAction(formData: FormData) {
+  const title = formData.get("title") as string;
+  const userId = formData.get("userId") as string;
+
+  const updatedAssignment: AssignmentUpdates = {
+    title,
+    userId: Number(userId)
+  };
+
+  const assignmentId = formData.get("assignmentId") as string;
+
+  const comment = !formData.get("comment") ? null : formData.get("comment") as string;
+  if (comment) updatedAssignment["comment"] = comment;
+
+  const score = !formData.get("score") ? null : formData.get("score") as string;
+  if (score) updatedAssignment["score"] = Number(score);
+
+  const tags: string[] = [];
+  const individualCommunication = formData.get(
+    "individualCommunication"
+  ) as string;
+  if (individualCommunication) tags.push("individualCommunication")
+
+  const teamCollaboration = formData.get("teamCollaboration") as string;
+  if (teamCollaboration) tags.push("teamCollaboration")
+
+  const management = formData.get("management") as string;
+  if (management) tags.push("management")
+
+  const design = formData.get("design") as string;
+  if (design) tags.push("design")
+
+  const backend = formData.get("backend") as string;
+  if (backend) tags.push("backend")
+
+  const frontend = formData.get("frontend") as string;
+  if (frontend) tags.push("frontend")
+
+    updatedAssignment["tags"] = tags;
+
+
+  scoresService.updateAssignment(Number(assignmentId), updatedAssignment);
 }
