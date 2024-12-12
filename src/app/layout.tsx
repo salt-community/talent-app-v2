006cfa92a359b31749a00aaeba9313801587a1e6
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-12 13:18:31 +0100

    rename header to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { roboto } from "@/components/ui/fonts";
-import { Header } from "@/components/header";
+import { Header } from "@/components/navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
