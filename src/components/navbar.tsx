f979385d1b706654600a009267261cecfac5e898
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-13 13:27:12 +0100

    remove comments
    Co-authored-by: Carl <carl@wbsweden.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,11 @@
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-// import { auth, clerkClient } from "@clerk/nextjs/server";
-// import Link from "next/link";
+
 export async function Header() {
-  // const { userId } = await auth();
-
-  // const user = userId
-  //   ? await (await clerkClient()).users.getUser(userId)
-  //   : null;
-
   return (
     <nav className="w-full h-10 px-8 shadow-sm sticky top-0 z-10 flex gap-2 bg-background justify-between items-center">
-      {/* <div>
-        {user && (
-          <span className="text-md">
-            Welcome to {"</salt>"}, {user.fullName}!
-          </span>
-        )}
-      </div> */}
       <Link href="/">
-      <span>&lt;salt/&gt;</span>
+        <span>&lt;salt/&gt;</span>
       </Link>
       <div className="flex gap-6">
         <div className="hover:bg-secondary py-1 px-2 rounded-lg text-sm">
@@ -32,8 +18,5 @@ export async function Header() {
         </SignedIn>
       </div>
     </nav>
-  ); }
- 
-
-
-    
+  );
+}
