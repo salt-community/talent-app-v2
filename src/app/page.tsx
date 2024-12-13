b96f6e259f499e4282d811141e80feb9814edc52
Odin Thif <mjd.thif@icloud.com>
2024-12-13 14:56:08 +0100

    removed userId and projects component from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { DeveloperCard } from "@/components/ui/developer-card";
 import { Background } from "@/features/backgrounds/components";
-import { Projects } from "@/features";
+
 import { ScoreBoard } from "@/features/scores";
 
 export default function Home() {
@@ -9,7 +9,6 @@ export default function Home() {
       <DeveloperCard>
         <Background id={1} />
         <ScoreBoard />
-        <Projects userId={"1"} />
       </DeveloperCard>
     </>
   );
