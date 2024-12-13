a8acca126d5ea8cc07b5c8ba30db089bc80dbcb8
CarlUdo <carl@wbsweden.com>
2024-12-13 15:26:22 +0100

    Add-value-attribute
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/components/form-text-area.tsx
+++ src/features/scores/components/form-text-area.tsx
@@ -1,18 +1,21 @@
+import { FormLabel } from "./form-label";
+
 type Props = {
   label: string;
+  value?: string | null;
+  handleChangeInput?: (inputValue: string, label: string) => void;
 };
 
-export function FormTextArea({ label }: Props) {
+export function FormTextArea({ label, value, handleChangeInput }: Props) {
   return (
     <div className="grid grid-cols-4 items-start gap-4">
-      <label
-        htmlFor={label.toLowerCase()}
-        className="text-right text-sm text-gray-700 mt-1"
-      >
-        {label}
-      </label>
+      <FormLabel label={label} />
       <textarea
         name={label.toLowerCase()}
+        {...(value ? { value } : {})}
+        onChange={(e) => {
+          if (handleChangeInput) handleChangeInput(e.target.value, label)
+        }}
         className="col-span-3 border border-gray-300 rounded-md p-2 text-sm focus:ring focus:ring-gray-200 focus:outline-none resize-none"
         rows={3}
         style={{ maxHeight: "150px", overflowY: "auto" }}
