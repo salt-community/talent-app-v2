5c98f5141d7a002e79ff8c6ca517f41cac67451b
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-13 12:22:03 +0100

    made-a-handle-change-inout-function
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/components/add-assignment.tsx
+++ src/features/scores/components/add-assignment.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ClipboardPlus } from 'lucide-react';
+import { ClipboardPlus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -12,17 +12,21 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { addAssigmentAction } from "../actions";
-import { useState } from 'react';
-import { CategoryCheckbox } from './category-checkbox';
-import { categoryTags } from '../categories';
-import { InputField } from './input-field';
-import { FormTextArea } from './form-text-area';
-import { FormLabel } from './form-label';
+import { useState } from "react";
+import { CategoryCheckbox } from "./category-checkbox";
+import { categoryTags } from "../categories";
+import { InputField } from "./input-field";
+import { FormTextArea } from "./form-text-area";
+import { FormLabel } from "./form-label";
 
 export function AddAssignment() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [title, setTitle] = useState("");
-  
+
+  const handleChangeInput = (inputValue: string, label: string) => {
+    if (label === "Title") setTitle(inputValue);
+  };
+
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogTrigger asChild>
@@ -42,16 +46,20 @@ export function AddAssignment() {
             Fill out the details below to add a new assignment.
           </DialogDescription>
         </DialogHeader>
-        <form action={addAssigmentAction} className="space-y-4">          
+        <form action={addAssigmentAction} className="space-y-4">
           <div className="grid gap-4">
-            <InputField label="Title" inputType="text" handleChangeTitle={setTitle} />
+            <InputField
+              label="Title"
+              inputType="text"
+              handleChangeInput={handleChangeInput}
+            />
             <InputField label="Score" inputType="number" />
             <FormTextArea label="Comment" />
             <div className="grid grid-cols-4 items-start gap-4">
               <FormLabel label="Tags" />
               <div className="col-span-3 grid grid-cols-2 gap-2">
                 {categoryTags.map((tag, index) => {
-                  return <CategoryCheckbox key={index} label={tag}/>
+                  return <CategoryCheckbox key={index} label={tag} />;
                 })}
               </div>
             </div>
