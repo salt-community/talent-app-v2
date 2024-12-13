c9663f936f7289404ec9192f69f23cdc0c82bd31
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-13 09:52:53 +0100

    feat(dashboard): export pool and create db with it

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const client = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle({ client });
 
 export type Db = typeof db;
