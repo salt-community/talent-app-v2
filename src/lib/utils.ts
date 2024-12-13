d3b1c208d14c5089f95cf9043d7d003a1c534b21
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-13 10:06:17 +0100

    added-util-function-for-camelcase
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,7 @@ export function capitalizeFirstLetter(str: string) {
   if (!str) return '';
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export function toCamelCase(str: string): string {
+  return str.split(' ').filter(word => word.trim() !== '').map((word, i) => i === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+}
