24cf615c3cfafa393fae5be3525b7381dbbdac07
CarlUdo <carl@wbsweden.com>
2024-12-13 15:26:22 +0100

    Add-value-attribute
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/components/input-field.tsx
+++ src/features/scores/components/input-field.tsx
@@ -3,16 +3,18 @@ import { FormLabel } from "./form-label";
 type Props = {
   label: string;
   inputType: "text" | "number";
+  value?: string | number;
   handleChangeInput?: (inputValue: string, label: string) => void;
 };
 
-export function InputField({ label, inputType, handleChangeInput }: Props) {
+export function InputField({ label, inputType, value, handleChangeInput }: Props) {
   return (
     <div className="grid grid-cols-4 items-center gap-4">
       <FormLabel label={label} />
       <input
         type={inputType}
         name={label.toLowerCase()}
+        value={value}
         className="col-span-3 border border-gray-300 rounded-md p-2 text-sm focus:ring focus:ring-gray-200 focus:outline-none"
         onChange={(e) => {
           if (handleChangeInput) handleChangeInput(e.target.value, label);
