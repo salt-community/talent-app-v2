37a6057f1cf57544ab5860b8e26b53bfd6d683cb
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-12-13 11:21:24 +0100

    added uuid to IAM schema

--- src/features/identity-access-management/schema.ts
+++ src/features/identity-access-management/schema.ts
@@ -1,9 +1,9 @@
-import { pgEnum, pgTable, integer } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, integer, uuid } from "drizzle-orm/pg-core";
 
 export const roles = pgEnum("roles", ["developer", "client", "core", "admin"]);
 
 export const identities = pgTable("identities", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid().primaryKey().notNull().default("gen_random_uuid()"),
   clerkId: integer("clerk_id"),
   roles: roles().notNull().default("developer"),
 });
