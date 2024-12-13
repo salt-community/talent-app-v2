625777a4a8b4fc5c58e622369af84c0a03793bd2
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-13 16:23:55 +0100

    route to profile when ypu click on profilcards

--- src/features/backgrounds/components/basic-info.tsx
+++ src/features/backgrounds/components/basic-info.tsx
@@ -1,12 +1,8 @@
-
 import { H2 } from "@/components";
 import { BackgroundAvatar } from "./avatar";
 import { Background } from "../types";
-import { SocialLink } from "./social-link";
-import { DialogForm } from "./dialog-form";
 
 type Props = { background: Background };
-
 
 export function BackgroundBasicInfo({ background }: Props) {
   const isFeatureBioEnabled = process.env.NEXT_PUBLIC_FEATURE_BIO === "ON";
@@ -16,16 +12,10 @@ export function BackgroundBasicInfo({ background }: Props) {
         <div className="flex gap-6 items-center">
           <BackgroundAvatar url={background.avatarUrl} />
           <div>
-            <p className="uppercase text-sm font-semibold">{background.title}</p>
-            <ul className="flex gap-1 justify-end items-top h-full">
-              {background.links &&
-                background.links.map((link) => (
-                  <li key={link.url} className="h-full flex justify-start">
-                    <SocialLink name={link.name} url={link.url} />
-                  </li>
-                ))}
-              <DialogForm background={background} />
-            </ul>
+            <p className="uppercase text-sm font-semibold">
+              {background.title}
+            </p>
+            <ul className="flex gap-1 justify-end items-top h-full"></ul>
             <H2>{background.name}</H2>
             {isFeatureBioEnabled && (
               <p className="font-light text-slate-600">{background.bio}</p>
