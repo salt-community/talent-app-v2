8d9b2c2ebd890adace801503d6f9861c271ff7d4
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-13 13:28:39 +0100

    make endpoint for id

---
+++ src/app/developers/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { DeveloperCard } from "@/components/ui/developer-card";
+import { Background } from "@/features/backgrounds/components";
+import { Projects } from "@/features";
+import { ScoreBoard } from "@/features/scores";
+
+type Params = {
+  id: string;
+};
+type Props = {
+  params: Params;
+};
+
+export default async function DeveloperDetailPage({ params }: Props) {
+  return (
+    <>
+      <DeveloperCard>
+        <Background id={1} />
+        <ScoreBoard />
+        <Projects id={params.id} />
+      </DeveloperCard>
+    </>
+  );
+}
