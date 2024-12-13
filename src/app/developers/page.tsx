f589965c72b3853b3b88b2fc37fcb68dde175ef5
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-13 15:46:08 +0100

    link to devloperspage

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent } from "@/components";
 import { Background } from "@/features/backgrounds/components";
 import { MeiliSearch } from "meilisearch";
 import { Search } from "./search";
+import Link from "next/link";
 
 type Props = { searchParams: Promise<{ search: string | undefined }> };
 export default async function Page({ searchParams }: Props) {
@@ -14,7 +15,7 @@ export default async function Page({ searchParams }: Props) {
 
   const index = client.index("backgrounds");
 
-  const ids = (await index.search(search)).hits.map((hit) => hit.id);
+  const ids = (await index.search(search)).hits.map((hit) => hit.uuid);
 
   return (
     <div className="p-4 sm:p-8 space-y-4">
@@ -22,11 +23,15 @@ export default async function Page({ searchParams }: Props) {
       <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {ids.map((id) => (
           <li key={id}>
-            <Card className="h-full mx-auto flex items-center justify-center">
-              <CardContent>
-                <Background />
-              </CardContent>
-            </Card>
+            <Link href={`/developers/${id}`}>
+              <div>
+                <Card className="h-full mx-auto flex items-center justify-center">
+                  <CardContent>
+                    <Background id={id} />
+                  </CardContent>
+                </Card>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
