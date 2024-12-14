039e4ceb2cc7bbf40bf710a03ed6796530421d63
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-14 11:54:35 +0100

    feat(background): style background

--- src/features/backgrounds/components/basic-info.tsx
+++ src/features/backgrounds/components/basic-info.tsx
@@ -9,28 +9,29 @@ export function BackgroundBasicInfo({ background }: Props) {
   const isFeatureBioEnabled = process.env.NEXT_PUBLIC_FEATURE_BIO === "ON";
   return (
     <>
-      <section className="flex justify-between">
-        <div className="flex gap-6 items-center">
+      <section className="flex justify-between w-full">
+        <div className="flex gap-6 items-center justify-between">
           <BackgroundAvatar url={background.avatarUrl} />
           <div>
             <p className="uppercase text-sm font-semibold">
               {background.title}
             </p>
-            <ul className="flex gap-1 justify-end items-top h-full">
-              {background.links &&
-                background.links.map((link) => (
-                  <li key={link.url} className="h-full flex justify-start">
-                    <SocialLink name={link.name} url={link.url} />
-                  </li>
-                ))}
-              <DialogForm background={background} />
-            </ul>
+
             <H2>{background.name}</H2>
             {isFeatureBioEnabled && (
               <p className="font-light text-slate-600">{background.bio}</p>
             )}
           </div>
         </div>
+        <ul className="flex gap-1 justify-end items-top h-full">
+          {background.links &&
+            background.links.map((link) => (
+              <li key={link.url} className="h-full flex justify-start">
+                <SocialLink name={link.name} url={link.url} />
+              </li>
+            ))}
+          <DialogForm background={background} />
+        </ul>
       </section>
     </>
   );
