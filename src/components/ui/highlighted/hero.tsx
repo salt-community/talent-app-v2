c31327ed4781413e12de40a5b921de1cc9323325
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-16 12:17:09 +0100

    style hero section

--- src/components/ui/highlighted/hero.tsx
+++ src/components/ui/highlighted/hero.tsx
@@ -2,12 +2,12 @@ import { H1, H2 } from "../header";
 
 export function Hero() {
   return (
-    <section className="outline h-96">
-      <H1>Lorem ipsum dolor sit</H1>
-      <H2 textColor="text-paragraph">
+    <section className="outline h-96 flex flex-col items-center justify-center gap-4 px-4">
+      <H1>Salt Talent App</H1>
+      <p className="text-paragraph text-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum et
         itaque minima.
-      </H2>
+      </p>
     </section>
   );
 }
