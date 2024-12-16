b0e1e43a2dd8e1908b1e43df22990a5a2f3c69ce
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-16 17:31:24 +0100

    add skill-highlight

--- src/components/ui/highlighted/highlighted-developers.tsx
+++ src/components/ui/highlighted/highlighted-developers.tsx
@@ -1,14 +1,16 @@
 import { ProjectHighlight } from "./project-highlight";
 import { QuoteHighlight } from "./quote-highlight";
+import { SkillsHighlight } from "./skills-highlight";
 import { VideoHighlight } from "./video-highlight";
 
 export function HighlightedDevelopers() {
   return (
-    <section className="flex flex-col justify-center py-8">
+    <section className="flex flex-col justify-center py-8 gap-4">
       <h2 className="text-center text-2xl font-bold mb-6">Developers</h2>
       <ProjectHighlight />
       <QuoteHighlight/>
       <VideoHighlight/>
+      <SkillsHighlight/>
     </section>
   );
 }
