eac470af1fd413ee79287946bc20f98c469514f4
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-16 16:10:57 +0100

    add quote highlight component

--- src/components/ui/highlighted/highlighted-developers.tsx
+++ src/components/ui/highlighted/highlighted-developers.tsx
@@ -1,10 +1,12 @@
 import { ProjectHighlight } from "./project-highlight";
+import { QuoteHighlight } from "./quote-highlight";
 
 export function HighlightedDevelopers() {
   return (
     <section className="flex flex-col justify-center py-8">
       <h2 className="text-center text-2xl font-bold mb-6">Developers</h2>
       <ProjectHighlight />
+      <QuoteHighlight/>
     </section>
   );
 }
