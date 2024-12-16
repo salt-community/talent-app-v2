1f175d39b062d8beb258153d7a64b31856a79020
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-16 13:50:28 +0100

    separate achievement-boards to a component

--- src/app/highlighted/page.tsx
+++ src/app/highlighted/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/ui/highlighted";
+import { AchievementBoards } from "@/components/ui/highlighted/achievement-boards";
 
 export default function Page() {
   return (
     <div>
       <Hero />
+      <AchievementBoards/>
     </div>
   );
 }
--- src/components/ui/highlighted/hero.tsx
+++ src/components/ui/highlighted/hero.tsx
@@ -4,47 +4,12 @@ import { H1, H3 } from "../header";
 export function Hero() {
   return (
     <>
-      <section className="outline h-96 flex flex-col items-center justify-center gap-4 px-4">
+      <section className=" h-96 flex flex-col items-center justify-center gap-4 px-4">
         <H1>Salt Talent App</H1>
         <p className="text-paragraph text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum et
           itaque minima.
         </p>
-      </section>
-      <section className="flex flex-col items-center my-8 gap-4">
-        <article className="outline rounded-full w-52 h-52 flex flex-col justify-center items-center p-3">
-          <h2 className="text-center text-xl font-bold">
-            Best Hack Week Project
-          </h2>
-          <div className="flex items-center p-2 gap-2">
-            <Avatar>
-              <AvatarImage className="w-10" src={"./avatar.png"} />
-            </Avatar>
-            <p className="text-center text-paragraph">Developer name</p>
-          </div>
-        </article>
-        <article className="outline  rounded-full w-52 h-52 flex flex-col justify-center items-center p-3">
-          <h2 className="text-center text-xl font-bold">
-            Best Final Project
-          </h2>
-          <div className="flex items-center p-2 gap-2">
-            <Avatar>
-              <AvatarImage className="w-10" src={"./avatar.png"} />
-            </Avatar>
-            <p className="text-center text-paragraph">Developer name</p>
-          </div>
-        </article>
-        <article className="outline rounded-full w-52 h-52 flex flex-col justify-center items-center p-3">
-          <h2 className="text-center text-xl font-bold">
-            Student of the month
-          </h2>
-          <div className="flex items-center p-2 gap-2">
-            <Avatar>
-              <AvatarImage className="w-10" src={"./avatar.png"} />
-            </Avatar>
-            <p className="text-center text-paragraph">Developer name</p>
-          </div>
-        </article>
       </section>
     </>
   );
