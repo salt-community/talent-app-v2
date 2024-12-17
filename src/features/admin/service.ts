4993868be897c924aa81d20ae1276925dd717545
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-12-17 16:32:05 +0100

    add checkaccess to admin feature

--- src/features/admin/service.ts
+++ src/features/admin/service.ts
@@ -14,13 +14,15 @@ export function createAdminService(
 ) {
   return {
     async getAllDeveloperProfiles() {
-      await checkAccess("admin.getAllDeveloperProfiles");
+      checkAccess("admin.getAllDeveloperProfiles");
       return await getAllDeveloperProfiles();
     },
     async deleteDeveloperProfile(id: string) {
+      checkAccess("admin.deleteDeveloperProfile");
       await deleteDeveloperProfile(id);
     },
     async updateStatus(id: string, status: DeveloperProfileStatus) {
+      checkAccess("admin.updateStatus");
       await updateStatus(id, status);
     },
   };
