a441342dd04e79f57000337af99fccc262a29829
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-17 15:18:31 +0100

    change get devs by id

--- src/components/ui/highlighted/quote-highlight.tsx
+++ src/components/ui/highlighted/quote-highlight.tsx
@@ -5,8 +5,10 @@ import { backgroundsService } from "@/features";
 import { BackgroundAvatar } from "@/features/backgrounds/components/avatar";
 
 export async function QuoteHighlight() {
-  const background = (
-    await backgroundsService.getByDevId("1170d801-e111-49ed-a52c-07d3dc4d3cfb"))[0];
+  const background = await backgroundsService.getBackgroundByDevId(
+    "1520dee4-7232-4422-9165-a4cd4aa52fa6"
+  );
+  if (!background) return null;
   return (
     <Card className="m-2 max-w-sm text-right rounded-2xl bg-neutral/10 px-4">
       <CardContent className="flex flex-col gap-6">
@@ -22,7 +24,7 @@ export async function QuoteHighlight() {
         </blockquote>
         <Link
           className="gap-2 flex justify-end w-full items-center"
-          href={`/developers/${background.devId}`}
+          href={`/developers/${background.avatarUrl}`}
         >
           <div className="flex flex-col gap-0">
             <p className="text-xl font-semibold ">{background.name}</p>
