be4204fa172563205e4c27ba475de87aece7269b
Odin Thif <mjd.thif@icloud.com>
2024-12-17 10:41:30 +0100

    renamed meiliseach file into meilisearch methods

---
+++ src/lib/meili-search/meili-methods.ts
@@ -0,0 +1,23 @@
+import { meiliSearch } from "./meili-search";
+
+export async function addBackgroundSearchIndex(background) {
+  const index = meiliSearch.index("backgrounds");
+  try {
+    const response = await index.addDocuments(background, {
+      primaryKey: "devId",
+    });
+    const task = await meiliSearch.waitForTask(response.taskUid);
+    if (task.status === "succeeded") {
+      console.log(
+        `Successfully seeded MeiliSearch index with ${task.details.indexedDocuments} backgrounds`
+      );
+    } else {
+      console.error(
+        `${task.status}: failed to seed MeiliSearch index with backgrounds because of errorzzz: ${task.error?.message}`
+      );
+    }
+    console.log("Resource added to Meilisearch:", response);
+  } catch (error) {
+    console.error("Error adding resource to Meilisearch:", error);
+  }
+}
