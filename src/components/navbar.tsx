8aa212d669ab8aed4de7a8f1d3c0b60f446d74f4
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-17 09:02:00 +0100

    get the userId from the clerkId

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,15 @@ import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 import Link from "next/link";
 import { HamburgerMenu } from "./ui/hamburger-menu";
+import { auth } from "@clerk/nextjs/server";
+import { iamService } from "@/features/iam/instance";
 
 export async function Header() {
+  const { userId } = await auth();
+  if (userId) {
+    const id = await iamService.getUserId(userId);
+    console.log(id);
+  }
   return (
     <nav className="w-full h-10 px-3 shadow-sm sticky top-0 z-10 flex bg-background justify-between items-center md:px-10">
       <Link href="/">
