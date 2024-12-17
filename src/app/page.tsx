28fb1b1417eb1f998023e258ec879b1562eb908f
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-17 16:23:44 +0100

    change home page
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,19 @@ export default async function Home({ searchParams }: Props) {
   const index = meiliSearch.index("backgrounds");
   const devIds = (await index.search(search)).hits.map((hit) => hit.devId);
   return (
-    <div className="p-4 sm:p-8 space-y-4">
+    <main className="px-4">
       <Search />
-      <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-        {devIds &&
-          devIds.map((devId) => (
-            <li key={devId} className="w-fit">
-              <Card className="p-2 h-full mx-auto flex items-center justify-center">
-                <CardContent>
-                  <Background devid={devId} />
-                </CardContent>
-              </Card>
-            </li>
-          ))}
+      <ul className="pt-14 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        {devIds.map((devId) => (
+          <li key={devId} className="">
+            <Card className="p-2 h-full flex justify-center min-w-[20rem]">
+              <CardContent>
+                <Background devid={devId} />
+              </CardContent>
+            </Card>
+          </li>
+        ))}
       </ul>
-    </div>
+    </main>
   );
 }
