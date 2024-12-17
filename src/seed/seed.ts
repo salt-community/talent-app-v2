6053c9cad0fe8b45a90cc1be514bfc5d403f48c0
Odin Thif <mjd.thif@icloud.com>
2024-12-17 10:40:33 +0100

    removed seedmeili search from seed.ts

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -2,12 +2,10 @@ import { backgroundsSeed } from "@/features/backgrounds/seed/seed";
 import { seedProjects } from "@/features/projects/seed";
 import { seedAssignments, seedIdentities } from "@/features";
 import { seedDeveloperProfiles } from "@/features";
-import { seedMeili } from "@/lib/meili-search";
 import { developerService } from "@/features/developer-profiles/instance";
 
 (async () => {
   console.log("Start seeding...");
-
   await seedIdentities();
   await seedDeveloperProfiles();
   const devIds = (await developerService.getAllDeveloperProfiles()).map(
@@ -17,7 +15,5 @@ import { developerService } from "@/features/developer-profiles/instance";
   await backgroundsSeed(devIds);
 
   await seedProjects();
-  await seedMeili();
-
   console.log("Done seeding...");
 })();
