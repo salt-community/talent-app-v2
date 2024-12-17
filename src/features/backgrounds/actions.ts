94d1721f6dd6675bdc6c3f554753d040cfd1fa1b
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-17 11:20:35 +0100

    feat(background/form): add zod validation

--- src/features/backgrounds/actions.ts
+++ src/features/backgrounds/actions.ts
@@ -2,27 +2,38 @@
 
 import { revalidatePath } from "next/cache";
 import { backgroundsService } from "./instance";
-import { BackgroundUpdate } from "./types";
+import { z } from "zod";
+
+const backgroundUpdate = z.object({
+  id: z.coerce.number(),
+  name: z.string().nonempty(),
+  title: z.string().nonempty(),
+  bio: z.string(),
+  skills: z
+    .string()
+    .transform((val) =>
+      JSON.parse(val).map((skill: { text: string }) => skill.text),
+    )
+    .pipe(z.array(z.string())),
+  languages: z
+    .string()
+    .transform((val) =>
+      JSON.parse(val).map((language: { text: string }) => language.text),
+    )
+    .pipe(z.array(z.string())),
+
+  educations: z
+    .string()
+    .transform((val) =>
+      JSON.parse(val).map((education: { text: string }) => education.text),
+    )
+    .pipe(z.array(z.string())),
+});
 
 export async function updateBackgroundAction(formData: FormData) {
-  const id = Number(formData.get("id"));
-  const name = formData.get("name") as string;
-  const title = formData.get("title") as string;
-  const bio = formData.get("bio") as string;
-  const skills = JSON.parse(formData.get("skills") as string);
-  const languages = JSON.parse(formData.get("languages") as string);
-  const educations = JSON.parse(formData.get("educations") as string);
+  const update = Object.fromEntries(formData.entries());
+  const validatedUpdate = backgroundUpdate.parse(update);
 
-  const background: BackgroundUpdate = {
-    id,
-    name,
-    title,
-    bio,
-    skills,
-    languages,
-    educations,
-  };
-
-  await backgroundsService.update(background);
+  await backgroundsService.update(validatedUpdate);
   revalidatePath("/");
 }
