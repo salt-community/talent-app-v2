9daf7c9c6021989c2590ee26a4ed090dff35b401
defycab <love.bellinder@gmail.com>
2024-12-17 15:23:28 +0100

    make a separate component for single background card view

---
+++ src/features/backgrounds/components/basic-info-card.tsx
@@ -0,0 +1,40 @@
+import { H2 } from "@/components";
+import { BackgroundAvatar } from "./avatar";
+import { Background } from "../types";
+import { SocialLink } from "./social-link";
+import { DialogForm } from "./dialog-form";
+
+type Props = { background: Background };
+
+export function BackgroundBasicInfoCard({ background }: Props) {
+  const isFeatureBioEnabled = process.env.NEXT_PUBLIC_FEATURE_BIO === "ON";
+
+  return (
+    <>
+      <section className="flex justify-between w-full">
+        <div className="flex gap-6 items-center justify-between">
+          <BackgroundAvatar url={background.avatarUrl} />
+          <div>
+            <p className="uppercase text-sm font-semibold">
+              {background.title}
+            </p>
+
+            <H2>{background.name}</H2>
+            {isFeatureBioEnabled && (
+              <p className="font-light text-slate-600">{background.bio}</p>
+            )}
+          </div>
+        </div>
+        <ul className="flex flex-col gap-1 items-center justify-end items-top h-full">
+          <DialogForm background={background} />
+          {background.links &&
+            background.links.map((link) => (
+              <li key={link.url} className="h-full flex justify-start">
+                <SocialLink name={link.name} url={link.url} />
+              </li>
+            ))}
+        </ul>
+      </section>
+    </>
+  );
+}
--- src/features/backgrounds/components/basic-info.tsx
+++ src/features/backgrounds/components/basic-info.tsx
@@ -1,42 +1,23 @@
 import { H2 } from "@/components";
 import { BackgroundAvatar } from "./avatar";
 import { Background } from "../types";
-import { SocialLink } from "./social-link";
-import { DialogForm } from "./dialog-form";
-import Link from "next/link";
 type Props = { background: Background };
 
 export function BackgroundBasicInfo({ background }: Props) {
   const isFeatureBioEnabled = process.env.NEXT_PUBLIC_FEATURE_BIO === "ON";
-  const devId = background.devId;
   return (
-    <>
-      <section className="flex justify-between w-full">
-        <Link href={`/developers/${devId}`}>
-          <div className="flex gap-6 items-center justify-between">
-            <BackgroundAvatar url={background.avatarUrl} />
-            <div>
-              <p className="uppercase text-sm font-semibold">
-                {background.title}
-              </p>
+    <section className="flex justify-between w-full">
+      <div className="flex gap-6 items-center justify-between">
+        <BackgroundAvatar url={background.avatarUrl} />
+        <div>
+          <p className="uppercase text-sm font-semibold">{background.title}</p>
 
-              <H2>{background.name}</H2>
-              {isFeatureBioEnabled && (
-                <p className="font-light text-slate-600">{background.bio}</p>
-              )}
-            </div>
-          </div>
-        </Link>
-        <ul className="flex flex-col gap-1 items-center justify-end items-top h-full">
-          <DialogForm background={background} />
-          {background.links &&
-            background.links.map((link) => (
-              <li key={link.url} className="h-full flex justify-start">
-                <SocialLink name={link.name} url={link.url} />
-              </li>
-            ))}
-        </ul>
-      </section>
-    </>
+          <H2>{background.name}</H2>
+          {isFeatureBioEnabled && (
+            <p className="font-light text-slate-600">{background.bio}</p>
+          )}
+        </div>
+      </div>
+    </section>
   );
 }
