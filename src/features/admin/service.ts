fd61f2f67deef281ec109558dd81810ab267af51
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-17 11:55:15 +0100

    updated function to be async

--- src/features/admin/service.ts
+++ src/features/admin/service.ts
@@ -2,15 +2,14 @@ import { Db } from "@/db";
 import { developerService } from "../developer-profiles/instance";
 import { iamService } from "../iam/instance";
 
-export function createAdminService(
+export async function createAdminService(
   db: Db,
   getAllDeveloperProfiles: typeof developerService.getAllDeveloperProfiles,
   hasAccess: typeof iamService.checkAccess
 ) {
   return {
-    getAllDeveloperProfiles() {
-      console.log(hasAccess("admin.getAllDeveloperProfiles"));
-      if (!hasAccess("admin.getAllDeveloperProfiles")) {
+    async getAllDeveloperProfiles() {
+      if (!(await hasAccess("admin.getAllDeveloperProfiles"))) {
         throw new Error("You do not have access to view developer profiles");
       }
       return getAllDeveloperProfiles();
