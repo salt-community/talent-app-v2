2a2cacc35ebe27f87d981cfaf668a0b962af153b
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-17 15:48:57 +0100

    updated basic-info
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/features/backgrounds/components/basic-info.tsx
+++ src/features/backgrounds/components/basic-info.tsx
@@ -6,35 +6,18 @@ type Props = { background: Background };
 export function BackgroundBasicInfo({ background }: Props) {
   const isFeatureBioEnabled = process.env.NEXT_PUBLIC_FEATURE_BIO === "ON";
   return (
-    <>
-      <section className="flex gap-3 justify-between w-full">
-        <Link href={`/developers/${devId}`}>
-          <div className="flex gap-6 justify-between">
-            <BackgroundAvatar url={background.avatarUrl} />
-            <div className="flex flex-col gap-1">
-              <p className="uppercase text-sm font-semibold">
-                {background.title}
-              </p>
+    <section className="flex justify-between w-full">
+      <div className="flex gap-6 items-center justify-between">
+        <BackgroundAvatar url={background.avatarUrl} />
+        <div>
+          <p className="uppercase text-sm font-semibold">{background.title}</p>
 
-              <H2>{background.name}</H2>
-              {isFeatureBioEnabled && (
-                <p className="font-light text-sm text-slate-600">
-                  {background.bio}
-                </p>
-              )}
-            </div>
-          </div>
-        </Link>
-        <ul className="flex flex-col gap-2 items-center justify-end items-top h-full">
-        <DialogForm background={background} />
-          {background.links &&
-            background.links.map((link) => (
-              <li key={link.url} className="h-full flex justify-start">
-                <SocialLink name={link.name} url={link.url} size={23} />
-              </li>
-            ))}
-        </ul>
-      </section>
-    </>
+          <H2>{background.name}</H2>
+          {isFeatureBioEnabled && (
+            <p className="font-light text-slate-600">{background.bio}</p>
+          )}
+        </div>
+      </div>
+    </section>
   );
 }
