7809ccf493a815b2d88ceba145a24ea7a9f6df7e
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-18 15:02:46 +0100

    import function from developer_profile to add new developer

--- src/features/iam/instance.ts
+++ src/features/iam/instance.ts
@@ -1,4 +1,5 @@
 import { db } from "@/db";
 import { createService } from "./service";
+import { developerService } from "../developer-profiles";
 
-export const iamService = createService(db);
+export const iamService = createService(db, developerService.add);
