b6dafe7eaeea6e8d8fbaee91f56004e062ee6b5e
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-18 14:36:58 +0100

    refactor(backgrounds/meili): move meili to backgrounds/meili

--- src/features/backgrounds/instance.ts
+++ src/features/backgrounds/instance.ts
@@ -1,8 +1,9 @@
 import { db } from "./db";
 import { createRepository } from "./repository";
 import { createBackgroundsService } from "./service";
-import { syncBackgroundSearchIndex } from "../../../meili-search";
 import { developerService } from "../developer-profiles";
+import { syncBackgroundSearchIndex } from "./meili";
+
 const serviceMethods = {
   syncBackgroundSearchIndex: syncBackgroundSearchIndex,
 };
