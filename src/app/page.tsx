e81f29897aab53d80a37b24d6bcca208656b0eb6
Odin Thif <mjd.thif@icloud.com>
2024-12-18 10:17:06 +0100

    rule enabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home({ searchParams }: Props) {
           <li key={devId} className="">
             <Card className="p-2 h-full flex justify-center min-w-[20rem]">
               <CardContent>
-                {/* //push force enbled */}
+                {/* //push force enbled rule */}
                 <Background devid={devId} />
               </CardContent>
             </Card>
