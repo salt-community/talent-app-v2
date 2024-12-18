5708e01209e3e278092e14f392a0244fa4ea42e0
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-18 14:28:20 +0100

    add highlight-page component
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ src/features/backgrounds/components/highlight-page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+import { HighlightedDevelopers } from "./highlighted-developers";
+
+export function HighlightPage(){
+ return(
+  <>
+  <Hero/>
+  <HighlightedDevelopers/>
+  </>
+ )
+}
