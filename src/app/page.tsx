d6dfd13a16a009620a2b4d8de82f69d6c6a4a0d5
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-18 15:55:26 +0100

    break out developer page to own component
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Card, CardContent } from "@/components";
 import {
-  Background,
   HighlightPage,
 } from "@/features";
 import { meiliSearch } from "../../meili-search";
 import { Search } from "./developers/search";
 import "dotenv/config"
+import { Developers } from "@/features";
 
 type Props = { searchParams: Promise<{ search: string | undefined }> };
 export default async function Home({ searchParams }: Props) {
@@ -14,24 +13,13 @@ export default async function Home({ searchParams }: Props) {
   const devIds = (await index.search(search)).hits.map((hit) => hit.devId);
 
   if (process.env.NEXT_PUBLIC_FF_HIGHLIGHTS === "ON") {
-    console.log("HI")
     return <HighlightPage />;
   }
 
   return (
     <main className="px-4">
       <Search />
-      <ul className="pt-14 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-        {devIds.map((devId) => (
-          <li key={devId} className="">
-            <Card className="p-2 h-full flex justify-center min-w-[20rem]">
-              <CardContent>
-                <Background devid={devId} />
-              </CardContent>
-            </Card>
-          </li>
-        ))}
-      </ul>
+      <Developers devIds={devIds}/>
     </main>
   );
 }
---
+++ src/features/backgrounds/components/developers.tsx
@@ -0,0 +1,23 @@
+import { Card, CardContent } from "@/components";
+import { Background } from "./background";
+
+type Props = {
+    devIds: string[];
+}
+
+export function Developers( { devIds}: Props) {
+    return (<>
+        <ul className="pt-14 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        {devIds.map((devId) => (
+          <li key={devId} className="">
+            <Card className="p-2 h-full justify-center min-w-[20rem]">
+              <CardContent >
+                <Background devid={devId} />
+              </CardContent>
+            </Card>
+          </li>
+        ))}
+      </ul>
+      </>
+    )
+}
