94280a50f0df069c3e396f6f2b46e2710f31338e
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-18 17:01:43 +0100

    change label and input on score edit form
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/components/form-text-area.tsx
+++ src/features/scores/components/form-text-area.tsx
@@ -1,4 +1,4 @@
-import { FormLabel } from "./form-label";
+import { Label, Textarea } from "@/components";
 
 type Props = {
   label: string;
@@ -8,18 +8,17 @@ type Props = {
 
 export function FormTextArea({ label, value, handleChangeInput }: Props) {
   return (
-    <div className="grid grid-cols-4 items-start gap-4">
-      <FormLabel label={label} />
-      <textarea
+    <>
+      <Label htmlFor={label.toLowerCase()}>{label}</Label>
+      <Textarea
         name={label.toLowerCase()}
         {...(value ? { value } : {})}
         onChange={(e) => {
           if (handleChangeInput) handleChangeInput(e.target.value, label);
         }}
-        className="col-span-3 border border-gray-300 rounded-md p-2 text-sm focus:ring focus:ring-gray-200 focus:outline-none resize-none"
         rows={3}
         style={{ maxHeight: "150px", overflowY: "auto" }}
       />
-    </div>
+    </>
   );
 }
