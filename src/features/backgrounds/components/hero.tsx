37cdd020e2cc11eb9dba4df03457c89ffda99c0b
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-19 12:39:06 +0100

    fix responsivity
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/backgrounds/components/hero.tsx
+++ src/features/backgrounds/components/hero.tsx
@@ -7,10 +7,10 @@ export function Hero() {
     <>
       <section className="flex flex-col items-center md:pr-96 justify-center text-center gap-4 md:py-52 py-32 bg-[url('/hero4.png')] bg-cover">
         <h1 className="text-xl uppercase">School of Applied Technology</h1>
-        <h2 className="text-transparent bg-clip-text bg-gradient-to-l from-[#f6806b] to-[#458cc6] text-6xl pb-4 px-40 font-bold">
+        <h2 className="text-transparent bg-clip-text bg-gradient-to-l from-[#f6806b] to-[#458cc6] text-6xl pb-4 md:px-40 font-bold">
           {"Tomorrow's Developers"}
         </h2>
-        <h2 className="text-5xl pb-4 px-40 font-bold">
+        <h2 className="text-5xl pb-4 md:px-40 px-8 font-bold">
           Available Today
         </h2>
         <Button>Sign In</Button>
