90c626d9f19b8dd3f84626d37bb170050fc41a75
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-19 15:27:50 +0100

    style homepage
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/backgrounds/components/hero.tsx
+++ src/features/backgrounds/components/hero.tsx
@@ -1,19 +1,16 @@
-import { Button } from "@/components";
-
-
-
 export function Hero() {
   return (
     <>
-      <section className="flex flex-col items-center md:pr-96 justify-center text-center gap-4 md:py-52 py-32 bg-[url('/hero4.png')] bg-cover">
-        <h1 className="text-xl uppercase">School of Applied Technology</h1>
-        <h2 className="text-transparent bg-clip-text bg-gradient-to-l from-[#f6806b] to-[#458cc6] text-6xl pb-4 md:px-40 font-bold">
+      <section className="flex flex-col items-center md:pr-96 justify-center text-center gap-4 md:py-60 py-44 md:bg-[url('/hero.jpg')] md:bg-cover">
+        <h1 className="md:text-xl  text-md uppercase">
+          School of Applied Technology
+        </h1>
+        <h2 className="text-transparent bg-clip-text bg-gradient-to-l from-[#f6806b] to-[#458cc6] md:text-6xl text-5xl md:px-40 font-bold">
           {"Tomorrow's Developers"}
         </h2>
-        <h2 className="text-5xl pb-4 md:px-40 px-8 font-bold">
+        <h2 className="md:text-5xl text-2xl pb-4 md:px-40 px-8 font-[600] text-paragraph">
           Available Today
         </h2>
-        <Button>Sign In</Button>
       </section>
     </>
   );
