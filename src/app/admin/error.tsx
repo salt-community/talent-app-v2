56cf8bf856368806a55033d32cbd91e9d567effc
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-12-20 15:09:16 +0100

    update error.tsx file for admin

--- src/app/admin/error.tsx
+++ src/app/admin/error.tsx
@@ -1,43 +1,26 @@
-'use client';
+"use client";
 
-import Link from "next/link";
+import { useEffect } from "react";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 
-type Props = {
-  error: Error;
-};
-
-export default function Error({ error }: Props) {
-  const errorMessage = error.message.split("\n").map((line, index) => {
-    const parts = line.split(":");
-    if (parts.length > 1) {
-      return (
-        <p key={index} className="mb-2">
-          <strong>{parts[0]}:</strong>
-          {parts.slice(1).join(":")}
-        </p>
-      );
-    }
-    return (
-      <p key={index} className="mb-2">
-        {line}
-      </p>
-    );
-  });
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-md max-w-lg w-full text-center">
-        <h2 className="text-3xl font-extrabold text-red-800 mb-6">Oops!</h2>
-        {error.message && (
-          <div className="text-red-600 font-medium mb-6">{errorMessage}</div>
-        )}        
-        <Link
-          href="/"
-          className="mt-6 rounded-md bg-blue-500 px-4 py-2 text-lg text-white transition-transform transform hover:scale-105"
-        >
-            Go back
-        </Link>
-      </div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Alert variant="destructive" className="max-w-md shadow-lg">
+        <AlertTitle>Access Denied</AlertTitle>
+        <AlertDescription>
+          You&apos;re not authorized to view this page. Please contact admin if
+          you believe this is an error.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
