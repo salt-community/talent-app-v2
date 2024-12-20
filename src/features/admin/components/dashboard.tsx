4f706825f12297b0b237ed5ca6c5c0fcffb9bde5
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-20 14:26:08 +0100

    created buttons to navigate at the admin page

--- src/features/admin/components/dashboard.tsx
+++ src/features/admin/components/dashboard.tsx
@@ -1,12 +1,27 @@
 import { H1 } from "@/components";
 import { adminService } from "../instance";
 import { DeveloperProfileList } from "./developer-profile-list";
+import Link from "next/link";
 
 export async function Dashboard() {
   const developers = await adminService.getAllDeveloperProfiles();
 
   return (
     <div className="container mx-auto flex flex-col justify-center px-4 pt-2 gap-4">
+      <div className="flex">
+        <Link
+          href={"/admin/developers"}
+          className="py-2 px-4 rounded-md border-solid border-2 border-black"
+        >
+          Developers
+        </Link>
+        <Link
+          href={"/admin/identities"}
+          className="mx-2 py-2 px-4 rounded-md  bg-black text-white"
+        >
+          Identities
+        </Link>
+      </div>
       <H1>Admin</H1>
       {developers.map((dev) => (
         <DeveloperProfileList
--- src/features/iam/components/dashboard-roles.tsx
+++ src/features/iam/components/dashboard-roles.tsx
@@ -1,11 +1,26 @@
 import { H1 } from "@/components";
 import IdentityProfileList from "./identity-profile-list";
 import { iamService } from "../instance";
+import Link from "next/link";
 
 export async function DashboardRoles() {
   const identities = await iamService.getAllIdentities();
   return (
     <div className="container mx-auto flex flex-col justify-center px-4 pt-2 gap-4">
+      <div className="flex">
+        <Link
+          href={"/admin/developers"}
+          className=" py-2 px-4 rounded-md bg-black text-white"
+        >
+          Developers
+        </Link>
+        <Link
+          href={"/admin/identities"}
+          className="mx-2 py-2 px-4 rounded-md border-solid border-2 border-black"
+        >
+          Identities
+        </Link>
+      </div>
       <H1>Admin</H1>
       {identities.map((identity) => (
         <IdentityProfileList
