a105c268e535c8b8963344ae3e2dcea459c01bbd
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-20 15:32:00 +0100

    change data to an arrat
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/features/backgrounds/components/posts.tsx
+++ src/features/backgrounds/components/posts.tsx
@@ -1,41 +1,52 @@
 import { InterviewCard } from "./interview-card";
-const victor = {
-  imageUrl: "/victor.png",
-  title: "MeiliSearch in Action",
-  excerpt:
-    "By integrating MeiliSearch into the Talent App, Victor Reyes has transformed how users find information, delivering faster and more accurate search results.",
-  avatarUrl: "/avatar.png",
-  name: "Victor Reyes",
-  date: "Dec 20, 2024",
-};
-
-const adrienn = {
-  imageUrl: "/adrienn.png",
-  title: "Designing for Inclusion in Technology",
-  excerpt:
-    "Exploring the role of accessibility in modern design and how inclusive practices shape better user experiences.",
-  avatarUrl: "/avatar.png",
-  name: "Adrienn Pozsgai",
-  date: "Dec 20, 2024",
-};
-const marten = {
-  imageUrl: "/marten.png",
-  title: "Building Access Management from Scratch",
-  excerpt:
-    "Mårten is tackling the challenge of creating a secure and logical access system while helping bring the Talent App to life from the ground up.",
-  avatarUrl: "/avatar.png",
-  name: "Mårten Söderlind",
-  date: "Dec 20, 2024",
-};
+export const highlightedDevelopers = [
+  {
+    id: "1",
+    imageUrl: "/victor.png",
+    title: "MeiliSearch in Action",
+    excerpt:
+      "By integrating MeiliSearch into the Talent App, Victor Reyes has transformed how users find information, delivering faster and more accurate search results.",
+    avatarUrl: "/avatar.png",
+    name: "Victor Reyes",
+    date: "Dec 20, 2024",
+  },
+  {
+    id: "2",
+    imageUrl: "/adrienn.png",
+    title: "Designing for Inclusion in Technology",
+    excerpt:
+      "Exploring the role of accessibility in modern design and how inclusive practices shape better user experiences.",
+    avatarUrl: "/avatar.png",
+    name: "Adrienn Pozsgai",
+    date: "Dec 20, 2024",
+  },
+  {
+    id: "3",
+    imageUrl: "/marten.png",
+    title: "Building Access Management from Scratch",
+    excerpt:
+      "Mårten is tackling the challenge of creating a secure and logical access system while helping bring the Talent App to life from the ground up.",
+    avatarUrl: "/avatar.png",
+    name: "Mårten Söderlind",
+    date: "Dec 20, 2024",
+  },
+];
 
 export function Posts() {
   return (
     <section className="bg-secondary pb-14">
-      <h2 className="text-center text-4xl font-semibold py-14">Our Developers in Focus</h2>
+      <h2 className="text-center text-4xl font-semibold py-14">
+        Our Developers in Focus
+      </h2>
       <article className="flex flex-wrap gap-10 justify-center">
-        <InterviewCard highlightedBackground={victor} />
-        <InterviewCard highlightedBackground={adrienn} />
-        <InterviewCard highlightedBackground={marten} />
+        {highlightedDevelopers.map((developer) => {
+          return (
+            <InterviewCard
+              key={developer.id}
+              highlightedBackground={developer}
+            />
+          );
+        })}
       </article>
     </section>
   );
