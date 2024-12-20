757f38a4a20e54e5aa61f62ed8b6dcc04118947f
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-20 15:52:08 +0100

    remove data from component file
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/features/backgrounds/components/posts.tsx
+++ src/features/backgrounds/components/posts.tsx
@@ -1,36 +1,4 @@
 import { InterviewCard } from "./interview-card";
-export const highlightedDevelopers = [
-  {
-    id: "1",
-    imageUrl: "/victor.png",
-    title: "MeiliSearch in Action",
-    excerpt:
-      "By integrating MeiliSearch into the Talent App, Victor Reyes has transformed how users find information, delivering faster and more accurate search results.",
-    avatarUrl: "/avatar.png",
-    name: "Victor Reyes",
-    date: "Dec 20, 2024",
-  },
-  {
-    id: "2",
-    imageUrl: "/adrienn.png",
-    title: "Designing for Inclusion in Technology",
-    excerpt:
-      "Exploring the role of accessibility in modern design and how inclusive practices shape better user experiences.",
-    avatarUrl: "/avatar.png",
-    name: "Adrienn Pozsgai",
-    date: "Dec 20, 2024",
-  },
-  {
-    id: "3",
-    imageUrl: "/marten.png",
-    title: "Building Access Management from Scratch",
-    excerpt:
-      "Mårten is tackling the challenge of creating a secure and logical access system while helping bring the Talent App to life from the ground up.",
-    avatarUrl: "/avatar.png",
-    name: "Mårten Söderlind",
-    date: "Dec 20, 2024",
-  },
-];
 
 export function Posts() {
   return (
