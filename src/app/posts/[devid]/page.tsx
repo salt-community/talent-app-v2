4d0500fd1984837da2ee9d2e094c416d5b13948c
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-20 11:05:33 +0100

    add post route and make post component

---
+++ src/app/posts/[devid]/page.tsx
@@ -0,0 +1,12 @@
+import { Post } from "@/features";
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const developerId = params.id;
+
+  console.log(developerId)
+  
+
+  return (
+   <Post/>
+  )}
