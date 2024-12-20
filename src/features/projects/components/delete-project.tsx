7a7d146f13645c7ba6056d358bba9d9fff855848
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-20 10:12:11 +0100

    updated delete message

--- src/features/projects/components/delete-project.tsx
+++ src/features/projects/components/delete-project.tsx
@@ -10,7 +10,7 @@ export default function DeleteProject({ onClick }: Props) {
   return (
     <AlertDialogDemo
       title={"Are you sure?"}
-      description={"This will delete your whole page!"}
+      description={"This will delete your project!"}
       onConfirm={onClick}
     >
       <Button>Delete</Button>
