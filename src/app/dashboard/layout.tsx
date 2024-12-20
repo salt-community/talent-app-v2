ef8e14689a832c09f2e066a4bad9c9ce47479787
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-12-20 09:27:12 +0100

    updated middleware to protect dashboard route

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,21 +1,7 @@
-import {
-  ClerkProvider,
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
-} from "@clerk/nextjs";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ClerkProvider>
-        <SignedIn>
-          <main className="w-full">{children}</main>
-        </SignedIn>
-        <SignedOut>
-          <RedirectToSignIn />
-        </SignedOut>
-      </ClerkProvider>
+      <main className="w-full">{children}</main>
     </>
   );
 }
