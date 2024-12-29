e716fd0314346445d50b68f070569ab8cd361ecc
CarlUdo <carl@wbsweden.com>
2024-12-29 18:26:20 +0100

    Update-scores-form-text-area-component
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/components/form-text-area.tsx
+++ src/features/scores/components/form-text-area.tsx
@@ -2,22 +2,26 @@ import { Label, Textarea } from "@/components";
 
 type Props = {
   label: string;
-  value?: string | null;
+  defaultValue?: string;
   handleChangeInput?: (inputValue: string, label: string) => void;
 };
 
-export function FormTextArea({ label, value, handleChangeInput }: Props) {
+export function FormTextArea({ 
+  label, 
+  defaultValue, 
+  handleChangeInput 
+}: Props) {
   return (
     <>
       <Label htmlFor={label.toLowerCase()}>{label}</Label>
       <Textarea
         name={label.toLowerCase()}
-        {...(value ? { value } : {})}
         onChange={(e) => {
           if (handleChangeInput) handleChangeInput(e.target.value, label);
         }}
         rows={3}
         style={{ maxHeight: "150px", overflowY: "auto" }}
+        defaultValue={defaultValue}
       />
     </>
   );
