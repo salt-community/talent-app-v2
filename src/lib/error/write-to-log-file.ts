0ec9a7f15a534d1fe7361e382163d0f9c65ec0e2
CarlUdo <carl@wbsweden.com>
2024-12-29 18:13:32 +0100

    Make-write-to-log-file-function
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ src/lib/error/write-to-log-file.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { appendFileSync } from "fs";
+import { formatDate } from "./format-date";
+import { getLogMessage } from "./get-log-message";
+import { CustomError } from "@/lib";
+
+export const writeToLogFile = (error: CustomError, errorText: string) => {
+  const date = new Date();
+  
+  const { todaysDateStripped } = formatDate(date);
+
+  const fileName = `error-log-${todaysDateStripped}.txt`; 
+   
+  const filePath = path.join(process.cwd(), "src/logs", fileName);
+
+  const logMessage = getLogMessage(error, errorText, date);
+
+	appendFileSync(filePath, logMessage);
+};
