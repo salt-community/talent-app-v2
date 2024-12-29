11fabe726531e4159e0410d9d524700a3ffa355d
CarlUdo <carl@wbsweden.com>
2024-12-29 18:12:47 +0100

    Make-error-handler-middleware
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ src/lib/error/error-handler.ts
@@ -0,0 +1,40 @@
+import {
+  CustomError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+} from "@/lib";
+import { writeToLogFile } from "./write-to-log-file";
+
+export const errorHandler = (error: unknown) => {  
+  let errorText =
+    `Title: Internal server error\n` +
+    `Status code: 500\n` +
+    `Message: Something went wrong`;
+  if (
+    error instanceof UnauthorizedError ||
+    error instanceof ForbiddenError ||
+    error instanceof NotFoundError
+  ) {
+    if (error instanceof UnauthorizedError) {
+      errorText =
+        `Title: ${error.title}\n` +
+        `Status code: ${error.statusCode}\n` +
+        `Message: ${error.message}. You're not authorized to view this page. Please contact admin if you believe this is an error.`;
+    } else {
+      errorText =
+        `Title: ${error.title}\n` +
+        `Status code: ${error.statusCode}\n` +
+        `Message: ${error.message}.`;
+    }
+  } else if (error instanceof Error) {
+    errorText =
+      `Title: Internal server error\n` +
+      `Status code: 500\n` +
+      `Message: ${error.message}`;
+  } else if (typeof error === "string" || typeof error === "number") {
+    error = new Error(error.toString());
+  }
+  writeToLogFile(error as CustomError, errorText);
+  throw new Error(errorText);
+};
