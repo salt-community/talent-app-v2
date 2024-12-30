73171250124a6e05f3652f0c12d5216871e86fa8
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-30 10:12:01 +0100

    rename

---
+++ src/features/backgrounds/components/hero-post.tsx
@@ -0,0 +1,12 @@
+export function HeroPost() {
+  return (
+    <>
+      <section className="flex flex-col justify-center  h-60">
+        <h1 className="text-4xl font-semibold ">
+          {"I am Adrienn Pozsgai"}
+        </h1>
+        <p className="text-paragraphLight">Passionate about solving real-world problems through code.</p>
+      </section>
+    </>
+  );
+}
