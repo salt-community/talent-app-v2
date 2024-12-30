b478f5595aa844b49b2a787d8adb6fc171eb544e
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-30 16:29:44 +0100

    Link to /developers/slug

--- src/features/backgrounds/components/background.tsx
+++ src/features/backgrounds/components/background.tsx
@@ -6,11 +6,13 @@ import { SkillsBadges } from "./skills-badges";
 type Props = { devid: string };
 export async function Background({ devid }: Props) {
   const background = await backgroundsService.getBackgroundByDevId(devid);
+  const slug = await backgroundsService.getSlugById(devid);
+
 
   if (!background) return null;
 
   return (
-    <Link href={`/developers/${devid}`}>
+    <Link href={`/developers/${slug}`}>
       <div className="space-y-2 max-w-96">
         <div className="flex justify-between items-start">
           <BackgroundBasicInfo background={background} />
