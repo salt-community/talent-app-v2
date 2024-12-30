94f7a6ad09dda7677b41e3b9570e03752676d024
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-30 10:57:52 +0100

    change the structure of the data
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/features/backgrounds/db/posts-data.ts
+++ src/features/backgrounds/db/posts-data.ts
@@ -1,6 +1,8 @@
 export const highlightedDevelopers = [
   {
     id: "1",
+    headline:
+      "Goal-oriented developer with a passion for creating impactful solutions",
     imageUrl: "/victor.png",
     title: "MeiliSearch in Action",
     excerpt:
@@ -8,38 +10,38 @@ export const highlightedDevelopers = [
     avatarUrl: "/avatar.png",
     name: "Victor Reyes",
     date: "Dec 20, 2024",
-    details: {
-      interviewTitle: "Optimizing Search with Victor Reyes",
-      questions: [
-        {
-          id: "q1",
-          question: "Why did you choose MeiliSearch for the Talent App?",
-          answer:
-            "MeiliSearch stood out for its balance between speed, relevance, and developer-friendliness. It was lightweight and easy to integrate, which made it the ideal choice for our needs.",
-        },
-        {
-          id: "q2",
-          question: "What challenges did you face during the integration?",
-          answer:
-            "One major challenge was handling real-time updates in search results without compromising speed. We had to fine-tune indexing to ensure scalability as the data grew.",
-        },
-        {
-          id: "q3",
-          question: "How does search improve the Talent App user experience?",
-          answer:
-            "Efficient search ensures users can find what they need quickly and accurately, which is critical in an app that matches talents to opportunities. It makes the app feel more intuitive and responsive.",
-        },
-        {
-          id: "q4",
-          question: "What advice do you have for developers implementing search functionality?",
-          answer:
-            "Always start with understanding your users' search behavior and data structure. Choose tools that fit your use case and focus on relevance and performance tuning.",
-        },
-      ],
-    },
+
+    questions: [
+      {
+        id: "q1",
+        question: "Why did you choose MeiliSearch for the Talent App?",
+        answer:
+          "MeiliSearch stood out for its balance between speed, relevance, and developer-friendliness. It was lightweight and easy to integrate, which made it the ideal choice for our needs.",
+      },
+      {
+        id: "q2",
+        question: "What challenges did you face during the integration?",
+        answer:
+          "One major challenge was handling real-time updates in search results without compromising speed. We had to fine-tune indexing to ensure scalability as the data grew.",
+      },
+      {
+        id: "q3",
+        question: "How does search improve the Talent App user experience?",
+        answer:
+          "Efficient search ensures users can find what they need quickly and accurately, which is critical in an app that matches talents to opportunities. It makes the app feel more intuitive and responsive.",
+      },
+      {
+        id: "q4",
+        question:
+          "What advice do you have for developers implementing search functionality?",
+        answer:
+          "Always start with understanding your users' search behavior and data structure. Choose tools that fit your use case and focus on relevance and performance tuning.",
+      },
+    ],
   },
   {
     id: "2",
+    headline: "Passionate about solving real-world problems through code.",
     imageUrl: "/adrienn.png",
     title: "Designing for Inclusion in Technology",
     excerpt:
@@ -47,44 +49,44 @@ export const highlightedDevelopers = [
     avatarUrl: "/avatar.png",
     name: "Adrienn Pozsgai",
     date: "Dec 20, 2024",
-    details: {
-      interviewTitle: "Creating Inclusive Design with Adrienn Pozsgai",
-      questions: [
-        {
-          id: "q1",
-          question: "What inspired your focus on accessibility in technology?",
-          answer:
-            "My background in special education showed me the transformative power of technology for people with disabilities. It fueled my drive to design for inclusivity.",
-        },
-        {
-          id: "q2",
-          question: "How does accessibility influence modern design?",
-          answer:
-            "It ensures no one is left behind. Accessibility improves usability for all users and creates better overall experiences.",
-        },
-        {
-          id: "q3",
-          question: "How did your experience at SALT impact your approach to accessibility?",
-          answer:
-            "SALT’s emphasis on teamwork, especially through mob programming, taught me the importance of collaboration. Accessibility is about considering diverse perspectives, and that aligns perfectly with the inclusive coding practices I learned at SALT.",
-        },
-        {
-          id: "q4",
-          question: "Why do you think accessibility is essential in modern design?",
-          answer:
-            "Accessibility is not just a feature; it’s a mindset. By designing for inclusivity, we make technology usable for all—not just those with disabilities but everyone. It’s about removing barriers and ensuring that no one is left out.",
-        },
-        {
-          id: "q5",
-          question: "What message would you share with aspiring developers?",
-          answer:
-            "Believe in your ability to grow and embrace challenges as opportunities. Focus on learning from every experience, and never underestimate the impact of inclusive design. It’s a skill that not only improves your work but also makes a real difference in people’s lives.",
-        },
-      ],
-    },
+    questions: [
+      {
+        id: "q1",
+        question: "What inspired your focus on accessibility in technology?",
+        answer:
+          "My background in special education showed me the transformative power of technology for people with disabilities. It fueled my drive to design for inclusivity.",
+      },
+      {
+        id: "q2",
+        question: "How does accessibility influence modern design?",
+        answer:
+          "It ensures no one is left behind. Accessibility improves usability for all users and creates better overall experiences.",
+      },
+      {
+        id: "q3",
+        question:
+          "How did your experience at SALT impact your approach to accessibility?",
+        answer:
+          "SALT’s emphasis on teamwork, especially through mob programming, taught me the importance of collaboration. Accessibility is about considering diverse perspectives, and that aligns perfectly with the inclusive coding practices I learned at SALT.",
+      },
+      {
+        id: "q4",
+        question:
+          "Why do you think accessibility is essential in modern design?",
+        answer:
+          "Accessibility is not just a feature; it’s a mindset. By designing for inclusivity, we make technology usable for all—not just those with disabilities but everyone. It’s about removing barriers and ensuring that no one is left out.",
+      },
+      {
+        id: "q5",
+        question: "What message would you share with aspiring developers?",
+        answer:
+          "Believe in your ability to grow and embrace challenges as opportunities. Focus on learning from every experience, and never underestimate the impact of inclusive design. It’s a skill that not only improves your work but also makes a real difference in people’s lives.",
+      },
+    ],
   },
   {
     id: "3",
+    headline: "Focusing on building a secure and logical access system",
     imageUrl: "/marten.png",
     title: "Building Access Management from Scratch",
     excerpt:
@@ -92,35 +94,32 @@ export const highlightedDevelopers = [
     avatarUrl: "/avatar.png",
     name: "Mårten Söderlind",
     date: "Dec 20, 2024",
-    details: {
-      interviewTitle: "Mårten’s Journey in Access Management",
-      questions: [
-        {
-          id: "q1",
-          question:
-            "What motivated you to build the access management system from scratch?",
-          answer:
-            "Starting from scratch allowed us to create a system tailored to the Talent App’s needs, ensuring both security and simplicity.",
-        },
-        {
-          id: "q2",
-          question: "What’s the most important aspect of access management?",
-          answer:
-            "Understanding user roles and permissions is crucial. A robust system ensures that every user has the appropriate level of access without compromising security.",
-        },
-        {
-          id: "q3",
-          question: "What challenges did you face during development?",
-          answer:
-            "Balancing security with usability was a challenge. We wanted a system that was secure but also intuitive for users and easy to maintain.",
-        },
-        {
-          id: "q4",
-          question: "What advice would you give for designing access systems?",
-          answer:
-            "Keep it simple and scalable. Plan for future growth and ensure the system can adapt to new requirements while staying user-friendly.",
-        },
-      ],
-    },
+    questions: [
+      {
+        id: "q1",
+        question:
+          "What motivated you to build the access management system from scratch?",
+        answer:
+          "Starting from scratch allowed us to create a system tailored to the Talent App’s needs, ensuring both security and simplicity.",
+      },
+      {
+        id: "q2",
+        question: "What’s the most important aspect of access management?",
+        answer:
+          "Understanding user roles and permissions is crucial. A robust system ensures that every user has the appropriate level of access without compromising security.",
+      },
+      {
+        id: "q3",
+        question: "What challenges did you face during development?",
+        answer:
+          "Balancing security with usability was a challenge. We wanted a system that was secure but also intuitive for users and easy to maintain.",
+      },
+      {
+        id: "q4",
+        question: "What advice would you give for designing access systems?",
+        answer:
+          "Keep it simple and scalable. Plan for future growth and ensure the system can adapt to new requirements while staying user-friendly.",
+      },
+    ],
   },
 ];
