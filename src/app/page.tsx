6a9a86a3373876820acea9804f0364a029eaf34d
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-30 14:09:39 +0100

    remove feature flag
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
 import { HighlightPage } from "@/features";
-import { Search } from "./developers/search";
-import { Developers } from "@/features";
-import { backgroundsService } from "@/features";
 
-type Props = { searchParams: Promise<{ search: string | undefined }> };
-export default async function Home({ searchParams }: Props) {
-  const search = (await searchParams).search;
-  const devIds = await backgroundsService.searchDevIds(search);
-
-  if (process.env.NEXT_PUBLIC_FF_HIGHLIGHTS === "ON") {
-    return <HighlightPage />;
-  }
-
-  return (
-    <main className="px-4">
-      <Search />
-      <Developers devIds={devIds} />
-    </main>
-  );
+export default async function Home() {
+  return <HighlightPage />;
 }
