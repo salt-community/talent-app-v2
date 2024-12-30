d965a2eb9e082a0c9db293fe31cd92cafb638351
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-30 10:32:37 +0100

    make height smaller
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/features/backgrounds/components/hero-post.tsx
+++ src/features/backgrounds/components/hero-post.tsx
@@ -1,7 +1,7 @@
 export function HeroPost() {
   return (
     <>
-      <section className="flex flex-col justify-center  h-60">
+      <section className="flex flex-col justify-center  h-44">
         <h1 className="text-4xl font-semibold ">
           {"I am Adrienn Pozsgai"}
         </h1>
