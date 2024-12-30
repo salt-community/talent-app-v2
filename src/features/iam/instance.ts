881711e1e2d5d513c2e77937b7fbbc3e082e8b65
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-30 14:44:22 +0100

    updated params

--- src/features/iam/instance.ts
+++ src/features/iam/instance.ts
@@ -1,8 +1,4 @@
 import { db } from "@/db";
 import { createService } from "./service";
 import { developerService } from "@/features/developer-profiles/instance";
-export const iamService = createService(
-  db,
-  developerService.add,
-  developerService.getById
-);
+export const iamService = createService(db, developerService.getById);
