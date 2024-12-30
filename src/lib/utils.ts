9e5d16898492cc81a33d6c205b8915d0f47bd1da
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-30 16:26:26 +0100

    add slugify helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,3 +21,11 @@ export function toCamelCase(str: string): string {
     )
     .join("");
 }
+
+export function slugify(name: string): string {
+  return name
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/^-+|-+$/g, '')
+      .substring(0, 50);
+}
