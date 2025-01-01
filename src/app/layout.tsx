af9d7076da15f0fc576b2703d5da947e284783e2
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2025-01-01 17:03:52 +0100

    fix footer to sit in bottom of the page on all the pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { roboto } from "@/components/ui/fonts";
 import { Header } from "@/components/navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
-import {Footer} from "@/components/footer";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Talent app",
@@ -20,12 +20,12 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${roboto.className} antialiased overscroll-none p-0`}>
-          <main className="space-y-1 w-full">
+          <div className="flex flex-col min-h-screen">
             <Header />
-            {children}
+            <main className="flex-grow">{children}</main>
             <Toaster />
-            <Footer/>
-          </main>
+            <Footer />
+          </div>
         </body>
       </html>
     </ClerkProvider>
