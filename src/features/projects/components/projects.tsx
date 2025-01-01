73f7a2966b866cca3d50f50c304c530a6304e88b
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-01-01 20:20:06 +0100

    make the projects responsive in different desktop sizes

--- src/features/projects/components/projects.tsx
+++ src/features/projects/components/projects.tsx
@@ -44,7 +44,7 @@ export async function Projects({ devId }: Props) {
   return (
     <div className="mt-4">
       <H2>Projects</H2>
-      <div className="flex flex-col justify-center mt-4 md:grid md:grid-cols-2 md:gap-8">
+      <div className="flex flex-col justify-center mt-4 lg:grid lg:grid-cols-2 md:gap-8">
         {projects.map((project) => (
           <div key={project.id} className="md:p-5 md:border md:rounded-md">
             <ProjectDetails project={project} editAccess={editAccess} />
