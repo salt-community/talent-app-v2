c03239e2b8d6263435e453af7ff15d3fb79b6f22
CarlUdo <carl@wbsweden.com>
2025-01-01 09:44:16 +0100

    Add error handling to background-card component

--- src/features/backgrounds/components/background-card.tsx
+++ src/features/backgrounds/components/background-card.tsx
@@ -3,10 +3,19 @@ import { backgroundsService } from "../instance";
 import { BackgroundBasicInfoCard } from "./basic-info-card";
 import { SkillsBadges } from "./skills-badges";
 import BackgroundSkeleton from "./background-skeleton";
+import type { Background } from "../types";
+import { CustomError, errorHandler } from "@/lib";
 
 type Props = { devid: string };
+
 export async function BackgroundCard({ devid }: Props) {
-  const background = await backgroundsService.getBackgroundByDevId(devid);
+  let background: Background | undefined;
+
+  try {
+    background = await backgroundsService.getBackgroundByDevId(devid);
+  } catch (error) {
+    errorHandler(error as CustomError)
+  }
 
   if (!background) {
     return (
