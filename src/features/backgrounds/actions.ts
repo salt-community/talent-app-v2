b3e9241756b2a5f1f80b6b775498d44bfaa3af54
CarlUdo <carl@wbsweden.com>
2025-01-01 15:55:19 +0100

    Add server side form validation

--- src/features/backgrounds/actions.ts
+++ src/features/backgrounds/actions.ts
@@ -1,17 +1,18 @@
 "use server";
 
 import { backgroundsService } from "./instance";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 import { redirect } from "next/navigation";
 import { SocialLink } from "./db";
 import { revalidatePath } from "next/cache";
+import { errorHandler } from "@/lib";
 
 const backgroundUpdate = z.object({
   id: z.coerce.number(),
   devId: z.string().nonempty(),
   avatarUrl: z.string().url().optional(),
-  name: z.string().nonempty(),
-  title: z.string().nonempty().optional(),
+  name: z.string().nonempty("Name must contain at least 1 character(s)"),
+  title: z.string().nonempty("Role must contain at least 1 character(s)").optional(),
   bio: z.string().optional(),
   github: z.string().optional(),
   cv: z.string().optional(),
@@ -34,22 +35,55 @@ const backgroundUpdate = z.object({
       JSON.parse(val).map((education: { text: string }) => education.text)
     )
     .pipe(z.array(z.string())),
-});
+  });
+  
+  type BackgroundUpdate = z.infer<typeof backgroundUpdate>;
 
-export async function updateBackgroundAction(
-  _: void | null | string,
-  formData: FormData
-) {
-  const update = Object.fromEntries(formData.entries());
-  const validatedUpdate = backgroundUpdate.parse(update);
-  const links: SocialLink[] = [
-    { name: "Github", url: validatedUpdate.github || "" },
-    { name: "Resume", url: validatedUpdate.cv || "" },
-  ];
-  await backgroundsService.update({ ...validatedUpdate, links });
+  type PreviousState = {
+    errorMessages: {
+      avatarUrlError?: string;
+      nameError?: string;
+      titleError?: string;      
+    };
+    update: {
+      [k: string]: FormDataEntryValue;
+    },  
+  } | undefined;
+  
+  
+  export async function updateBackgroundAction(_: PreviousState, formData: FormData) {
+    const update = Object.fromEntries(formData.entries());
+    let validatedUpdate: BackgroundUpdate | undefined;
 
-  revalidatePath("/");
-  redirect(`/developers/${validatedUpdate.devId}`);
+    try {
+      validatedUpdate = backgroundUpdate.parse(update);
+      const links: SocialLink[] = [
+        { name: "Github", url: validatedUpdate.github || "" },
+        { name: "Resume", url: validatedUpdate.cv || "" },
+      ];
+      await backgroundsService.update({ ...validatedUpdate, links });      
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const avatarUrlError = error.flatten().fieldErrors.avatarUrl?.[0];
+        const nameError = error.flatten().fieldErrors.name?.[0];
+        const titleError = error.flatten().fieldErrors.title?.[0];
+
+        return {
+          errorMessages: {
+            avatarUrlError,
+            nameError,
+            titleError
+          },
+          update
+        }
+      }
+      errorHandler(error);
+    }
+
+    if (validatedUpdate) { 
+      revalidatePath("/"); 
+      redirect(`/developers/${validatedUpdate.devId}`); 
+    }
 }
 
 export async function addBackground(id: string) {
