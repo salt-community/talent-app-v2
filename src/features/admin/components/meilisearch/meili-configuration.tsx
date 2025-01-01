6360c9029f72f089af560feb5018697565908b67
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-01 17:19:58 +0100

    feat(admin/meili): display MeiliSynonyms in MeiliSettings component

--- src/features/admin/components/meilisearch/meili-configuration.tsx
+++ src/features/admin/components/meilisearch/meili-configuration.tsx
@@ -3,14 +3,17 @@ import { updateMeilisearchSettingsAction } from "../../action";
 import { LoadingButton } from "./loading-button";
 import { H3 } from "@/components";
 import { adminService } from "../../instance";
+import { MeiliSynonyms } from "./meili-synonyms";
 
 export async function MeiliSettings() {
   const settings = await adminService.getMeilisearchSettings();
 
+  const synonyms = Object.entries(settings.synonyms ?? {});
   return (
     <div className="space-y-4">
       <H3>Meilisearch Settings</H3>
       <Form action={updateMeilisearchSettingsAction}>
+        <MeiliSynonyms synonyms={synonyms} />
         <LoadingButton text="Update" loadingText="Saving settings..." />
       </Form>
     </div>
