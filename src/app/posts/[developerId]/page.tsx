21a50a94466e37a4bd586d3bb70440e089b4af59
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-01-01 19:41:36 +0100

    make the post recive the right developer id

--- src/app/posts/[developerId]/page.tsx
+++ src/app/posts/[developerId]/page.tsx
@@ -1,10 +1,10 @@
 import { Post } from "@/features";
 
 export default async function Page(props: {
-  params: Promise<{ devId: string }>;
+  params: Promise<{ developerId: string }>;
 }) {
   const params = await props.params;
-  const developerId = params.devId;
+  const developerId = params.developerId;
 
   return <Post developerId={developerId} />;
 }
