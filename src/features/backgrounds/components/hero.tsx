2df1cd0c55a595016446cc470e2db38202825103
Marcus Rådell <marcus@radell.net>
2025-01-10 16:23:40 +0100

    optimize superlarge image

--- src/features/backgrounds/components/hero.tsx
+++ src/features/backgrounds/components/hero.tsx
@@ -1,16 +1,26 @@
+import Image from "next/image";
+
 export function Hero() {
   return (
     <>
-      <section className="flex flex-col items-center md:pr-96 justify-center text-center gap-4 md:py-60 py-40 bg-[url('/hero.jpg')] bg-[length:210%_auto] md:bg-cover md:bg-center">
-        <h1 className="md:text-xl  text-md uppercase">
-          School of Applied Technology
-        </h1>
-        <h2 className="text-transparent bg-clip-text bg-gradient-to-l from-[#f6806b] to-[#458cc6] md:text-6xl text-5xl md:px-40 font-bold">
-          {"Tomorrow's Developers"}
-        </h2>
-        <h2 className="md:text-5xl text-2xl pb-4 md:px-40 px-8 font-[600] text-paragraph">
-          Available Today
-        </h2>
+      <section className="relative flex flex-col items-center md:pr-96 justify-center text-center gap-4 md:py-60 py-40">
+        <Image
+          src="/hero.jpg"
+          alt="Hero background"
+          className="object-cover"
+          fill
+        />
+        <div className="z-10">
+          <h1 className=" md:text-xl  text-md uppercase">
+            School of Applied Technology
+          </h1>
+          <h2 className=" text-transparent bg-clip-text bg-gradient-to-l from-[#f6806b] to-[#458cc6] md:text-6xl text-5xl md:px-40 font-bold">
+            {"Tomorrow's Developers"}
+          </h2>
+          <h2 className=" md:text-5xl text-2xl pb-4 md:px-40 px-8 font-[600] text-paragraph">
+            Available Today
+          </h2>
+        </div>
       </section>
     </>
   );
