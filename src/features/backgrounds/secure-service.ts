8f062b878c8e35100c736e3be1ca44661ade0434
Marcus Rådell <marcus@radell.net>
2025-01-14 15:47:06 +0100

    secureService will now return a secured service

--- src/features/backgrounds/secure-service.ts
+++ src/features/backgrounds/secure-service.ts
@@ -1,3 +1,5 @@
+import { iamService } from "../iam";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Service = { [key: string]: (args: any) => Promise<any> };
 
@@ -11,19 +13,25 @@ export type PermissionsSchema<
 export function secureService<
   TFeatureName extends string,
   TService extends Service,
->(
-  featureName: TFeatureName,
-  service: TService
-): PermissionsSchema<TFeatureName, TService> {
+>(featureName: TFeatureName, service: TService): TService {
   const serviceMethodNames = Object.keys(service) as Array<
     keyof TService extends string ? keyof TService : never
   >;
 
-  return serviceMethodNames.reduce(
-    (acc, serviceMethodName) => {
-      acc[serviceMethodName] = `${featureName}.${serviceMethodName}`;
-      return acc;
-    },
-    {} as PermissionsSchema<TFeatureName, TService>
-  );
+  const securedService = serviceMethodNames.reduce((acc, serviceMethodName) => {
+    const permission = `${featureName}.${serviceMethodName}` as const;
+
+    async function securedServiceMethod(args: any) {
+      await iamService.checkAccess(permission as any);
+
+      return service[serviceMethodName](args);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    acc[serviceMethodName] = securedServiceMethod as any;
+
+    return acc;
+  }, {} as TService);
+
+  return securedService;
 }
