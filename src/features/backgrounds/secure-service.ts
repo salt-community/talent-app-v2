4438d5bb858226ff1e5758b287ca6163e2a9cc68
Marcus Rådell <marcus@radell.net>
2025-01-14 15:13:07 +0100

    secureService generates a permissionsSchema

--- src/features/backgrounds/secure-service.ts
+++ src/features/backgrounds/secure-service.ts
@@ -1,7 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Service = { [key: string]: (args: any) => Promise<any> };
+
 export type PermissionsSchema<
-  TNamespace extends string,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  TService extends { [key: string]: (args: any) => Promise<any> },
+  TFeatureName extends string,
+  TService extends Service,
 > = {
-  [K in keyof TService]: `${TNamespace}.${K extends string ? K : never}`;
+  [K in keyof TService]: `${TFeatureName}.${K extends string ? K : never}`;
 };
+
+export function secureService<
+  TFeatureName extends string,
+  TService extends Service,
+>(
+  featureName: TFeatureName,
+  service: TService
+): PermissionsSchema<TFeatureName, TService> {
+  const serviceMethodNames = Object.keys(service) as Array<
+    keyof TService extends string ? keyof TService : never
+  >;
+
+  return serviceMethodNames.reduce(
+    (acc, serviceMethodName) => {
+      acc[serviceMethodName] = `${featureName}.${serviceMethodName}`;
+      return acc;
+    },
+    {} as PermissionsSchema<TFeatureName, TService>
+  );
+}
