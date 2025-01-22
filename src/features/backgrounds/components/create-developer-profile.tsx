ac3c89dfe644ce479ef216e5126b1f183b577ab2
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-01-22 10:07:42 +0100

    add margin for Developer Profiles card for the screens starting from md size

--- src/features/backgrounds/components/create-developer-profile.tsx
+++ src/features/backgrounds/components/create-developer-profile.tsx
@@ -29,7 +29,7 @@ export async function CreateDeveloperProfileCard({
         <CreateProfileButton identityId={identityId} />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 mx-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-4">
         {devIds.map((devId, index) => (
           <DeveloperProfileCard key={index} devId={devId.id} />
         ))}
