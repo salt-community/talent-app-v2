05045856e20a32e4d3209ffae164a8463bfe65d6
Junita Berglin <junita.berglin@gmail.com>
2025-01-22 15:26:25 +0100

    ✨(Add): assignment dashboard

--- src/components/ui/assignments-dashboard.tsx
+++ src/components/ui/assignments-dashboard.tsx
@@ -2,21 +2,31 @@
 
 import * as React from "react";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Separator } from "./separator";
 
 export function AssignmentsDashboard() {
   const [tab, setTab] = React.useState("assignments");
 
   return (
-    <section className="p-6">
-      <h1 className="mb-4 text-xl font-bold">Instructor Dashboard</h1>
+    <section className="p-4">
+      <h1 className="mb-4 text-center text-3xl font-extralight p-8">
+        Instructor Dashboard
+      </h1>
 
-      <Tabs value={tab} onValueChange={setTab}>
-        <TabsList>
-          <TabsTrigger value="assignments">Assignments</TabsTrigger>
-          <TabsTrigger value="scoring">Scoring</TabsTrigger>
-          <TabsTrigger value="statistics">Statistics</TabsTrigger>
+      <Tabs value={tab} onValueChange={setTab} className="text-center">
+        <TabsList className="gap-4 p-4">
+          <TabsTrigger value="assignments" className="px-6 py-2 text-l">
+            Assignments
+          </TabsTrigger>
+          <TabsTrigger value="scoring" className="px-6 py-2 text-l">
+            Scoring
+          </TabsTrigger>
+          <TabsTrigger value="statistics" className="px-6 py-2 text-l">
+            Statistics
+          </TabsTrigger>
         </TabsList>
       </Tabs>
+      <Separator className="my-5" />
     </section>
   );
 }
