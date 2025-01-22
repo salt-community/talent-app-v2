2ef10932fc555f586fb925e37dc6f8ba65933e96
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-01-22 15:55:43 +0100

    Refactor CreateProfileButton to use useCallback for fetchProfiles to optimize dependency management and ensure proper state handling

--- src/features/backgrounds/components/create-profile-button.tsx
+++ src/features/backgrounds/components/create-profile-button.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { AlertDialogDemo, Button } from "@/components";
 import { useRouter } from "next/navigation";
-import { addDeveloperProfileAction, getDeveloperProfilesAction } from "../actions";
-import { useEffect, useState } from "react";
+import {
+  addDeveloperProfileAction,
+  getDeveloperProfilesAction,
+} from "../actions";
+import { useCallback, useEffect, useState } from "react";
 
 interface Props {
   identityId: string;
@@ -10,18 +13,23 @@ interface Props {
 
 export function CreateProfileButton({ identityId: identityId }: Props) {
   const router = useRouter();
-  const [hasProfile, setHasProfile] = useState(false)
+  const [hasProfile, setHasProfile] = useState(false);
+
+  const fetchProfiles = useCallback(async () => {
+    const profiles = await getDeveloperProfilesAction(identityId);
+    setHasProfile((profiles?.length || 0) > 0);
+  }, [identityId]);
+
   const addProfile = async () => {
     await addDeveloperProfileAction(identityId);
+    fetchProfiles();
     router.refresh();
   };
+
   useEffect(() => {
-    const fetchProfiles = async() => {
-      const profiles = await getDeveloperProfilesAction(identityId)
-      setHasProfile((profiles?.length || 0) > 0);
-    }
-    fetchProfiles()
-  }, [identityId])
+    fetchProfiles();
+  }, [fetchProfiles]);
+
   return (
     <AlertDialogDemo
       title={"Confirmation Required"}
