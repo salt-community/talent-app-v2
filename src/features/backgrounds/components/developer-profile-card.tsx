8f6c7473cce052b067a5d412b8cd6c83da6478af
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-01-23 10:35:16 +0100

    remove delete btn from DeveloperProfileCard to prevent the developer profile to be deleted

--- src/features/backgrounds/components/developer-profile-card.tsx
+++ src/features/backgrounds/components/developer-profile-card.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent } from "@/components";
 import React from "react";
 import { Background } from "./background";
-import { DeleteProfileButton } from "./delete-profile-button";
 
 type Props = {
   devId: string;
@@ -13,9 +12,6 @@ export function DeveloperProfileCard({ devId }: Props) {
       <ul className="pb-10 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
         <li className="">
           <Card className="p-2 h-full justify-center min-w-[20rem]">
-            <div className="flex justify-end md:hover:cursor-pointer">
-              <DeleteProfileButton devId={devId} />
-            </div>
             <CardContent>
               <Background devId={devId} />
             </CardContent>
