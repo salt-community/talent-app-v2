1d56a1cf9bcb8cd4ba23b84f94043a905bf94e8d
Junita Berglin <junita.berglin@gmail.com>
2025-01-24 13:03:33 +0100

    ✨(Add): utility functions for form data processing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { AssignmentFormData } from "@/features";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -17,7 +18,26 @@ export function toCamelCase(str: string): string {
     .map((word, i) =>
       i === 0
         ? word.toLowerCase()
-        : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+        : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
     )
     .join("");
+}
+export function getFormData(formData: FormData): AssignmentFormData {
+  const devId = formData.get("devId") as string | null;
+  const title = formData.get("title") as string | null;
+  const score = formData.get("score") as string | null;
+  const comment = formData.get("comment") as string | null;
+  const tagsString = formData.get("tags") as string | null;
+  const cohort = formData.get("cohort") as string | null;
+  const description = formData.get("description") as string | null;
+
+  return {
+    devId: devId ?? undefined,
+    title: title ?? undefined,
+    score: score ?? undefined,
+    comment: comment ?? undefined,
+    tags: tagsString ? tagsString.split(",") : undefined,
+    cohort: cohort ?? undefined,
+    description: description ?? undefined,
+  };
 }
