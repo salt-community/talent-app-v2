83effc413f84c2c465a325dd847f5b3c948888dc
Junita Berglin <junita.berglin@gmail.com>
2025-01-24 11:13:28 +0100

    🗃️ (Add): `cohort` to Drizzle schema (`developerProfiles` table)

--- src/features/developer-profiles/schema.ts
+++ src/features/developer-profiles/schema.ts
@@ -15,6 +15,7 @@ export const developerProfiles = pgTable("developer_profiles", {
   name: varchar().notNull(),
   email: varchar().notNull(),
   status: developerStatuses().notNull().default("unpublished"),
+  cohort: varchar("cohort").notNull().default(""),
 });
 
 export type DeveloperProfileSelect = typeof developerProfiles.$inferSelect;
