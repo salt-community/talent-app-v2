ec6c104d0315e43a74631caad73879fc6499a965
Junita Berglin <junita.berglin@gmail.com>
2025-01-25 17:46:25 +0100

    🗃️ (Add): Add 'cohortId' in identities table

--- src/features/iam/schema.ts
+++ src/features/iam/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, pgEnum, uuid, varchar } from "drizzle-orm/pg-core";
+import { cohorts } from "../backgrounds";
 
 export const roles = pgEnum("roles", ["developer", "core", "admin"]);
 export type Roles = typeof roles;
@@ -9,6 +10,9 @@ export const identities = pgTable("identities", {
   name: varchar("name").notNull(),
   email: varchar("email").notNull(),
   role: roles().notNull().default("developer"),
+  cohortId: uuid("cohort_id").references(() => cohorts.id, {
+    onDelete: "set null",
+  }),
 });
 
 export type IdentitySelect = typeof identities.$inferSelect;
