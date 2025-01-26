d07dc5b036b2ed573d17d6b35d02c7b3747b9ca7
Junita Berglin <junita.berglin@gmail.com>
2025-01-26 12:20:09 +0100

    🚚 (remove): 'addCohortAssignment' from 'assignments action'

--- src/features/assignments/actions.ts
+++ src/features/assignments/actions.ts
@@ -6,7 +6,6 @@ import { getFormData } from "./utils";
 import type { NewAssignment } from "./types";
 import { ZodError } from "zod";
 import { errorHandler } from "@/lib";
-import { developerProfilesService } from "../developer-profiles";
 
 type PreviousState =
   | {
@@ -107,39 +106,4 @@ export async function deleteAssignmentAction(id: number) {
   }
 
   revalidatePath("/developers");
-}
-
-export async function addCohortAssignmentAction(
-  _: PreviousState,
-  formData: FormData
-): Promise<void | { errorMessages: { titleError?: string } }> {
-  const { cohort, title, description, tags } = getFormData(formData) as {
-    cohort: string;
-    title: string;
-    description: string;
-    tags: string[];
-  };
-
-  try {
-    const devIds = await developerProfilesService.getAllByCohort(cohort);
-    const promises = devIds.map(async (devId: string) => {
-      await assignmentsService.addAssignment({
-        devId,
-        title,
-        score: 0,
-        comment: description,
-        tags,
-      });
-    });
-
-    await Promise.all(promises);
-  } catch (error) {
-    if (error instanceof ZodError) {
-      return {
-        errorMessages: { titleError: error.flatten().fieldErrors.title?.[0] },
-      };
-    }
-    errorHandler(error);
-  }
-  revalidatePath("/assignments");
 }
