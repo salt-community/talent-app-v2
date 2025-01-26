ef1dc557ddc38f401a6eaea60b3ca7b5fb8f1bad
Junita Berglin <junita.berglin@gmail.com>
2025-01-26 14:51:30 +0100

    🏷️ (Update): assignment types to fit new tables

--- src/features/assignments/types.ts
+++ src/features/assignments/types.ts
@@ -2,23 +2,40 @@ import { createAssignmentsService } from "./service";
 
 export type Assignment = {
   id: number;
-  devId: string;
   title: string;
+  tags: string[];
+  cohortId: string;
+  comment?: string;
+  categories?: string[];
+  createdAt: Date;
+};
+
+export type AssignmentScore = {
+  id: string;
+  assignmentId: string;
+  identityId: string;
   score: number;
   comment?: string;
-  tags?: string[];
+  createdAt: Date;
 };
 
 export type AssignmentFormData = {
-  devId?: string;
-  title?: string;
-  score?: string;
+  title: string;
+  tags: string[];
+  cohortId: string;
   comment?: string;
-  tags?: string[];
-  cohort?: string;
+  categories?: string[];
   description?: string;
 };
 
-export type NewAssignment = Omit<Assignment, "id">;
+export type AssignmentScoreFormData = {
+  assignmentId: string;
+  identityId: string;
+  score: number;
+  comment?: string;
+};
+
+export type NewAssignment = Omit<Assignment, "id" | "createdAt">;
+export type NewAssignmentScore = Omit<AssignmentScore, "id" | "createdAt">;
 
 export type AssignmentsService = ReturnType<typeof createAssignmentsService>;
