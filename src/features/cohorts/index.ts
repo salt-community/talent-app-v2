be9d7ed9b193003318d16808b891b5578c84bdae
Junita Berglin <junita.berglin@gmail.com>
2025-01-27 15:56:18 +0100

    🎨 (Add): export cohort in features/index.ts

---
+++ src/features/cohorts/index.ts
@@ -0,0 +1,5 @@
+export * from "./seed";
+export * from "./types";
+export * from "./instance";
+export * from "./schema";
+export * from "./service";
--- src/features/index.ts
+++ src/features/index.ts
@@ -3,3 +3,4 @@ export * from "./developer-profiles";
 export * from "./backgrounds";
 export * from "./projects";
 export * from "./assignments";
+export * from "./cohorts";
