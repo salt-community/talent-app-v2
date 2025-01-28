10dfa7f5c03213553911a20b4ff02de5ded17372
Junita Berglin <junita.berglin@gmail.com>
2025-01-28 13:21:10 +0100

    ✨(change): logic to fit new types

--- src/features/assignments/logic.ts
+++ src/features/assignments/logic.ts
@@ -1,26 +1,44 @@
-import { Assignment } from "./types";
+import { Assignment, AssignmentScore } from "./types";
 
-export const calculateAverageAssignmentsScore = (assignments: Assignment[]) => {
-  const scores = assignments.map((assignment) => assignment.score);
+export const calculateAverageAssignmentsScore = (
+  assignmentScores: AssignmentScore[]
+): number => {
+  const scores = assignmentScores
+    .map((assignment) => Number(assignment.score))
+    .filter((score) => !isNaN(score));
+
+  if (scores.length === 0) return 0;
+
   const averageAssignmentScore =
     scores.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
-      0,
+      0
     ) / scores.length;
+
   return Math.round(averageAssignmentScore);
 };
 
 export const calculateAverageCategoryScore = (
+  assignmentScores: AssignmentScore[],
   assignments: Assignment[],
-  category: string,
-) => {
-  const scores = assignments
+  category: string
+): number => {
+  const categoryAssignmentIds = assignments
     .filter((assignment) => (assignment.tags ?? []).includes(category))
-    .map((assignment) => assignment.score);
-  const averageAssignmentScore =
+    .map((assignment) => assignment.id);
+
+  const scores = assignmentScores
+    .filter((score) => categoryAssignmentIds.includes(score.assignmentId))
+    .map((assignmentScore) => Number(assignmentScore.score))
+    .filter((score) => !isNaN(score));
+
+  if (scores.length === 0) return 0;
+
+  const averageCategoryScore =
     scores.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
-      0,
+      0
     ) / scores.length;
-  return isNaN(averageAssignmentScore) ? 0 : Math.round(averageAssignmentScore);
+
+  return Math.round(averageCategoryScore);
 };
