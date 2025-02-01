a9fb4ae6327e6b6ab80c8f4a4b48814681a532b2
Junita Berglin <junita.berglin@gmail.com>
2025-02-01 12:16:43 +0100

    ♻️ (ref): developer data being loaded in admin/page.tsx (instead of admin/developer/page.tsx

--- src/app/admin/developers/page.tsx
+++ src/app/admin/developers/page.tsx
@@ -1,13 +1,5 @@
-import { developerProfilesService } from "@/features";
 import { Dashboard } from "@/features/admin";
-import { DevelopersProvider } from "@/features/developer-profiles/developer-context";
 
-export default async function AdminDevelopersPage() {
-  const developers = await developerProfilesService.getAll();
-
-  return (
-    <DevelopersProvider initialDevelopers={developers}>
-      <Dashboard />
-    </DevelopersProvider>
-  );
+export default function AdminDevelopersPage() {
+  return <Dashboard />;
 }
