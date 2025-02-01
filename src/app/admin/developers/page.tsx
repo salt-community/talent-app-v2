a4f3bf37547f4358480839ebf86fa0abaf8c7853
Junita Berglin <junita.berglin@gmail.com>
2025-02-01 12:02:11 +0100

    (Add): DeveloperProvider inside 'Page'/'DeveloperPage'

--- src/app/admin/developers/page.tsx
+++ src/app/admin/developers/page.tsx
@@ -1,5 +1,13 @@
-import { Dashboard } from "@/features/admin/components";
+import { developerProfilesService } from "@/features";
+import { Dashboard } from "@/features/admin";
+import { DevelopersProvider } from "@/features/developer-profiles/developer-context";
 
-export default function Page() {
-  return <Dashboard />;
+export default async function AdminDevelopersPage() {
+  const developers = await developerProfilesService.getAll();
+
+  return (
+    <DevelopersProvider initialDevelopers={developers}>
+      <Dashboard />
+    </DevelopersProvider>
+  );
 }
