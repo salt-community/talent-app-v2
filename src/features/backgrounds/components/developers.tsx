9888444c561558051c500f25bdbe34a95b55ae6b
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-06 10:05:48 +0100

    fix: update key prop in Developers component to use index for mapping

--- src/features/backgrounds/components/developers.tsx
+++ src/features/backgrounds/components/developers.tsx
@@ -9,8 +9,8 @@ export function Developers({ devIds }: Props) {
   return (
     <>
       <ul className="pt-20 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-        {devIds.map((devId) => (
-          <li key={devId} className="">
+        {devIds.map((devId, index) => (
+          <li key={index} className="">
             <Card className="p-2 h-full justify-center min-w-[20rem]">
               <CardContent>
                 <Background devId={devId} />
