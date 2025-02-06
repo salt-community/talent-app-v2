cf26a48af8d3f99e43041c7d953aab758a6c1efa
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-06 11:50:11 +0100

    add title and style to highlighted developers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,22 @@ import {
   HighlightPage,
 } from "@/features";
 import { HighlightedDevelopers } from "@/features";
+import { Hero } from "@/features/backgrounds/components/hero";
 
 export default async function Home() {
 
   return process.env.FF_NEW_HIGHLIGHTS === "ON" ? (
-    <>
-      <h2>Developer in Focus</h2>
-      <h2>Talented, driven, and making a difference.</h2>
-      <div className="px-4">
+    <div className="bg-zinc-50">
+    <Hero/>
+      <h2 className="text-center text-4xl font-extrabold pt-16 text-header">Developers in Focus</h2>
+      <h3 className="text-center text-lg pt-4 text-paragraphLight px-1">
+        Each developer has a unique story, skill set, and impact on the tech
+        world. Meet a standout talent and explore their journey
+      </h3>
+      <div className="px-4 pb-12">
         <HighlightedDevelopers />
       </div>
-    </>
+    </div>
   ) : (
     <HighlightPage />
   );
