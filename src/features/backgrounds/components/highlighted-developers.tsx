793fe3d3bcc92e2dcebf6c0b159739421286cd8a
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-06 16:01:08 +0100

    fix build problem with feature flagging
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/features/backgrounds/components/highlighted-developers.tsx
+++ src/features/backgrounds/components/highlighted-developers.tsx
@@ -1,22 +1,17 @@
 import { Card, CardContent } from "@/components";
-import { backgroundsService } from "@/features";
 import { Background } from "./background";
-import { errorHandler } from "@/lib";
 
-export async function HighlightedDevelopers() {
-  let getHighlightedDeveloperProfileIds: string[] = [];
 
-  try {
-    getHighlightedDeveloperProfileIds =
-      await backgroundsService.getHighlightedDeveloperProfileIds();
-  } catch (error) {
-    errorHandler(error);
-  }
+type Props = {
+ developerProfileIds: string[]
+}
+
+export async function HighlightedDevelopers({developerProfileIds}: Props) {
 
   return (
     <>
       <ul className="px-8 pt-10 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-        {getHighlightedDeveloperProfileIds.map((developerProfileId, index) => (
+        {developerProfileIds.map((developerProfileId, index) => (
           <li key={index} className="">
             <Card className="p-2 h-full flex justify-center min-w-[20rem]">
               <CardContent>
