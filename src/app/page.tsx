5acdb4e87af82d40be30ac773d8f4219f047ca25
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-06 11:25:28 +0100

    add highlighted developers component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import {
-  Developers,
-  developerProfilesService,
   HighlightPage,
 } from "@/features";
+import { HighlightedDevelopers } from "@/features";
 
 export default async function Home() {
-  const highlightedDeveloperProfileIds =
-    await developerProfilesService.getHighlightedDevIds();
 
   return process.env.FF_NEW_HIGHLIGHTS === "ON" ? (
-    <div className="container mx-auto flex flex-col justify-center px-4 py-4 gap-4">
-      <Developers devIds={highlightedDeveloperProfileIds} />
-    </div>
+    <>
+      <h2>Developer in Focus</h2>
+      <h2>Talented, driven, and making a difference.</h2>
+      <div className="px-4">
+        <HighlightedDevelopers />
+      </div>
+    </>
   ) : (
     <HighlightPage />
   );
