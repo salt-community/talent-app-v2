6e119e10eee4ed329452c88a069c7f7dd3aedb65
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-10 11:37:15 +0100

    correct implementation of tabs

--- src/features/admin/components/instructors-view/assignments-view/assignments-view.tsx
+++ src/features/admin/components/instructors-view/assignments-view/assignments-view.tsx
@@ -1,4 +1,3 @@
-
 import {
   Accordion,
   AccordionItem,
@@ -7,34 +6,39 @@ import {
 } from "@/components/ui/accordion";
 import { Tabs, TabsTrigger } from "@/components/ui/tabs";
 import { Tag } from "lucide-react";
-import { TabsContent } from "@radix-ui/react-tabs";
+import { TabsContent, TabsList } from "@radix-ui/react-tabs";
 import { Assignment, getAllAssignments } from "@/features/assignments";
+import { AddAssignmentForm } from "./add-assignment";
 
 export async function ListAssignments() {
-
-  const assignments =await getAllAssignments()
+  const assignments = await getAllAssignments();
   if (!assignments) {
     return <p>No assignments found.</p>;
   }
 
   return (
-    <div className="m-6">
-      <Tabs>
-        <TabsContent value="assignments">
-          <TabsTrigger value="add">Add Assignment</TabsTrigger>
-        </TabsContent>
-      </Tabs>
-      <Accordion type="single" collapsible>
-        {assignments.map((assignment:Assignment) => (
-          <AccordionItem key={assignment.id} value={assignment.id}>
-            <AccordionTrigger>{assignment.title}</AccordionTrigger>
-            <AccordionContent>
-              <p>Comment: {assignment.comment}</p>
-              <Tag>Tags: {assignment.tags.join(", ")}</Tag>
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </div>
+    <Tabs defaultValue="assignments" className="m-6">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="assignments">Assignments</TabsTrigger>
+        <TabsTrigger value="add">Add Assignment</TabsTrigger>
+      </TabsList>
+      <TabsContent value="assignments">
+        <Accordion type="single" collapsible>
+          {assignments.map((assignment: Assignment) => (
+            <AccordionItem key={assignment.id} value={assignment.id}>
+              <AccordionTrigger>{assignment.title}</AccordionTrigger>
+              <AccordionContent>
+                <p>Comment: {assignment.comment}</p>
+                <Tag>Tags: {assignment.tags.join(", ")}</Tag>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </TabsContent>
+      <TabsContent value="add">
+        <p>add</p>
+        {/* <AddAssignmentForm /> */}
+      </TabsContent>
+    </Tabs>
   );
 }
