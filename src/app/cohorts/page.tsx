f649c81852e2c73810f6a12454236259dd8e4684
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-11 11:42:38 +0100

    created route for cohorts

---
+++ src/app/cohorts/page.tsx
@@ -0,0 +1,9 @@
+import { CohortDashboard } from "@/features/cohorts/components/cohort-dashboard";
+
+export default async function Page() {
+  return (
+    <div>
+      <CohortDashboard />
+    </div>
+  );
+}
