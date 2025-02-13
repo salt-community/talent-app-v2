70449248a046cf630fa1223d7a341c01e4d38a78
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-13 14:41:43 +0100

    changed back schema change

--- src/features/developer-profiles/schema.ts
+++ src/features/developer-profiles/schema.ts
@@ -5,7 +5,7 @@ export const developerProfiles = pgTable("developer_profiles", {
   id: uuid()
     .primaryKey()
     .default(sql`gen_random_uuid()`),
-  identityId: varchar("identity_id").notNull(),
+  identityId: varchar("identity_id").default("1"),
   name: varchar().notNull(),
   email: varchar().notNull(),
   status: varchar().notNull().default("unpublished"),
