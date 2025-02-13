e106ff16122ae6c4c4c9ce4443cdab65107953c5
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-13 14:10:45 +0100

    change style

--- src/features/cohorts/components/cohort-dashboard.tsx
+++ src/features/cohorts/components/cohort-dashboard.tsx
@@ -4,7 +4,7 @@ import { AddCohortForm } from "./create-cohort-form";
 export function CohortDashboard() {
   return (
     <>
-      <h1 className="text-center text-2xl py-6 font-bold">Cohorts</h1>
+      <h1 className="text-center text-3xl py-6 font-bold">Cohorts</h1>
       <div className="flex justify-center">
         <CohortList />
       </div>
