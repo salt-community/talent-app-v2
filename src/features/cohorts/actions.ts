d00ddfa9bf57acd013d371af0361199208f9898b
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-13 10:23:21 +0100

    delete unused code

--- src/features/cohorts/actions.ts
+++ src/features/cohorts/actions.ts
@@ -1,56 +1,14 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-// import { ReactNode } from "react";
-// import { ZodError } from "zod";
 import { errorHandler } from "@/lib";
 import { cohortsService } from "./instance";
 import { CohortFormData } from "./types";
-
-// export async function addCohortAction(
-//   _: unknown,
-//   formData: FormData
-// ): Promise<
-//   | {
-//       successMessage: ReactNode;
-//       errorMessages?: {
-//         titleError?: string;
-//       };
-//       newCohort?: CohortFormData;
-//     }
-//   | undefined
-// > {
-//   const { name, description, status } = getCohortFormData(formData);
-
-//   const newCohort: CohortFormData = {
-//     name,
-//     description: description || "",
-//     status: status as "planned",
-//   };
-
-//   try {
-//     await cohortsService.createCohort(newCohort);
-//   } catch (error) {
-//     if (error instanceof ZodError) {
-//       const titleError = error.flatten().fieldErrors.title?.[0];
-//       return {
-//         successMessage: null,
-//         errorMessages: { titleError },
-//         newCohort: newCohort,
-//       };
-//     }
-//     errorHandler(error);
-//   }
-
-//   revalidatePath("/cohorts");
-// }
 
 export async function addCohortAction(cohort: CohortFormData) {
   await cohortsService.createCohort(cohort);
   revalidatePath("/cohorts");
 }
-
-
 
 export async function fetchCohortsAction() {
   try {
