b7a155e4c62644b69a7f9fd3194d2db5705a9e43
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-13 10:51:15 +0100

    updated imports and naming

--- src/features/cohorts/components/cohort-list.tsx
+++ src/features/cohorts/components/cohort-list.tsx
@@ -5,24 +5,24 @@ import {
   AccordionTrigger,
 } from "@/components";
 import { fetchCohortsAction } from "../actions";
-import CohortSelect from "./cohort-select";
 import CohortDevelopers from "./cohort-developers";
-import CohortFormWrapper from "./cohort-form-wraper";
+import CohortFormWrapper from "./cohort-form-wrapper";
+import { Cohort } from "../types";
 
 export async function CohortList() {
   const cohorts = await fetchCohortsAction();
   return (
     <Accordion type="single" collapsible className="w-11/12 md:w-5/6 lg:w-4/6">
-      {cohorts.map((cohort) => (
-        <AccordionItem key={cohort.id} value={cohort.id}>
+      {cohorts.map((cohort: Cohort) => (
+        <AccordionItem key={cohort.id} value={cohort.id!}>
           <AccordionTrigger>{cohort.name}</AccordionTrigger>
           <AccordionContent>
             <div className="flex justify-between">
               <p>{cohort.description}</p>
               <p>{cohort.status}</p>
             </div>
-            <CohortFormWrapper cohortId={cohort.id} />
-            <CohortDevelopers cohortId={cohort.id} />
+            <CohortFormWrapper cohortId={cohort.id!} />
+            <CohortDevelopers cohortId={cohort.id!} />
           </AccordionContent>
         </AccordionItem>
       ))}
