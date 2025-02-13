5ebbe8b42fc5922fa7b7534c917ab997e91a5643
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-13 10:15:31 +0100

    separate zod validation for add cohort

---
+++ src/features/cohorts/validation.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+
+export const addCohortFormSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  description: z.string().optional(),
+  status: z.enum(["planned", "ongoing", "finished"]),
+});
