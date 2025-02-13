865384a891491dcb1ef820f69c81853d75722293
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-13 10:39:55 +0100

    updated component to a form

--- src/features/cohorts/components/cohort-select.tsx
+++ src/features/cohorts/components/cohort-select.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Select,
   SelectContent,
@@ -8,27 +9,41 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { getAllUnassignedDevelopers } from "../actions";
+import { UnassignedDevelopers } from "../types";
+import { useState } from "react";
 
 type Props = {
   cohortId: string;
+  UnassignedDevelopers: UnassignedDevelopers[];
 };
-export default async function CohortSelect({ cohortId }: Props) {
-  const UnassignedDevelopers = await getAllUnassignedDevelopers();
+export default function CohortSelect({
+  cohortId,
+  UnassignedDevelopers,
+}: Props) {
+  const [selectedDeveloper, setSelectedDeveloper] = useState("");
+
+  async function handleSubmit() {
+    await addDeveloperToCohort(cohortId, selectedDeveloper);
+  }
   return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a student" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Developers</SelectLabel>
-          {UnassignedDevelopers.map((developer, index) => (
-            <SelectItem value={developer.id} key={index}>
-              {developer.name}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <form action={handleSubmit}>
+      <Select value={selectedDeveloper} onValueChange={setSelectedDeveloper}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a student" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Developers</SelectLabel>
+            {UnassignedDevelopers.map((developer, index) => (
+              <SelectItem value={developer.id} key={index}>
+                {developer.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <input type="hidden" name="cohortId" value={selectedDeveloper} />
+      <button type="submit">Add</button>
+    </form>
   );
 }
