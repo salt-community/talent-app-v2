bee6202369b9f549fa8be4a039dbc4f140ecb922
Marcus Rådell <marcus@radell.net>
2025-02-13 12:43:36 +0100

    export insecure assignment service for seeding consumption

--- src/features/assignments/instance.ts
+++ src/features/assignments/instance.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { createAssignmentsService } from "./service";
 import { secureService } from "../iam";
 
-const insecureAssignmentService = createAssignmentsService(db);
+export const insecureAssignmentService = createAssignmentsService(db);
 
 export const assignmentsService = secureService(
   "assignments",
