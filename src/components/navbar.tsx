4a9d5c72774c62125298f0318c154d855d628b1d
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-13 15:49:36 +0100

    navbar visible only if you sign in

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,26 +1,26 @@
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
-
 import Link from "next/link";
 import { HamburgerMenu } from "./ui/hamburger-menu";
 import { iamService } from "@/features";
 
 export async function Header() {
   const user = await iamService.controlUser();
+
   return (
     <nav className="w-full h-12 px-3 shadow-sm sticky top-0 z-20 flex bg-background justify-between items-center md:px-10">
       <Link href="/">
         <span>&lt;/salt&gt;</span>
       </Link>
       <div className="flex items-center gap-4 md:gap-6">
-        <div className="border-b border-white hover:border-primary  text-sm">
-          <SignedOut>
+        <SignedOut>
+          <div className="border-b border-white hover:border-primary text-sm">
             <SignInButton />
-          </SignedOut>
-        </div>
+          </div>
+        </SignedOut>
         <SignedIn>
           <UserButton />
+          <HamburgerMenu user={user} />
         </SignedIn>
-        <HamburgerMenu user={user} />
       </div>
     </nav>
   );
