c610480024c4b137a13654cc6613310d39045b37
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-14 09:58:22 +0100

    removed old migration files

---
+++ drizzle/0000_thick_ultimates.sql
@@ -0,0 +1,131 @@
+CREATE TYPE "public"."roles" AS ENUM('developer', 'core', 'admin');--> statement-breakpoint
+CREATE TYPE "public"."operation" AS ENUM('upsert', 'delete');--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "assignment_scores" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"assignment_id" uuid,
+	"identity_id" uuid,
+	"score" integer DEFAULT 0,
+	"comment" varchar DEFAULT '',
+	"created_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "assignments" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"cohort_id" uuid NOT NULL,
+	"title" varchar NOT NULL,
+	"comment" varchar DEFAULT '',
+	"categories" varchar[] DEFAULT '{}',
+	"created_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "cohort_identities" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"cohort_id" uuid NOT NULL,
+	"identity_id" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "cohorts" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar NOT NULL,
+	"status" varchar DEFAULT 'planned' NOT NULL,
+	"description" varchar DEFAULT 'assignment' NOT NULL,
+	"created_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "developer_profiles" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"identity_id" varchar DEFAULT '1',
+	"name" varchar NOT NULL,
+	"email" varchar NOT NULL,
+	"status" varchar DEFAULT 'unpublished' NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "identities" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"clerk_id" varchar NOT NULL,
+	"name" varchar NOT NULL,
+	"email" varchar NOT NULL,
+	"role" varchar DEFAULT 'developer' NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "backgrounds" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "backgrounds_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"developer_profile_id" uuid NOT NULL,
+	"avatar_url" varchar DEFAULT '' NOT NULL,
+	"name" varchar NOT NULL,
+	"title" varchar NOT NULL,
+	"bio" varchar NOT NULL,
+	"links" jsonb NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "background_educations" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "background_educations_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"background_id" integer NOT NULL,
+	"name" varchar NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "background_languages" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "background_languages_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"background_id" integer NOT NULL,
+	"name" varchar NOT NULL,
+	"level" integer DEFAULT 5 NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "meili_search_outbox" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "meili_search_outbox_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"developer_profile_id" uuid NOT NULL,
+	"operation" "operation" NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "background_skills" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "background_skills_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"background_id" integer NOT NULL,
+	"name" varchar NOT NULL,
+	"level" integer DEFAULT 5 NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "projects" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"username" varchar NOT NULL,
+	"repository" varchar NOT NULL,
+	"title" varchar NOT NULL,
+	"image_url" varchar(500),
+	"image_alt" varchar,
+	"project_website" varchar,
+	"description" varchar NOT NULL,
+	"performance" varchar NOT NULL,
+	"commits" varchar NOT NULL,
+	"last_commits" varchar,
+	"issues" varchar NOT NULL,
+	"userId" uuid DEFAULT gen_random_uuid() NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "assignment_scores" ADD CONSTRAINT "assignment_scores_assignment_id_assignments_id_fk" FOREIGN KEY ("assignment_id") REFERENCES "public"."assignments"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "cohort_identities" ADD CONSTRAINT "cohort_identities_cohort_id_cohorts_id_fk" FOREIGN KEY ("cohort_id") REFERENCES "public"."cohorts"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "background_educations" ADD CONSTRAINT "background_educations_background_id_backgrounds_id_fk" FOREIGN KEY ("background_id") REFERENCES "public"."backgrounds"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "background_languages" ADD CONSTRAINT "background_languages_background_id_backgrounds_id_fk" FOREIGN KEY ("background_id") REFERENCES "public"."backgrounds"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "background_skills" ADD CONSTRAINT "background_skills_background_id_backgrounds_id_fk" FOREIGN KEY ("background_id") REFERENCES "public"."backgrounds"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- drizzle/0003_clever_multiple_man.sql
+++
@@ -1,5 +0,0 @@
-DO $$ BEGIN
- ALTER TABLE "assignment_scores" ADD CONSTRAINT "assignment_scores_assignment_id_assignments_id_fk" FOREIGN KEY ("assignment_id") REFERENCES "public"."assignments"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
