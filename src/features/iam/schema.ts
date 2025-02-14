6fd48111a79f5d674b496111cbd938124d1878aa
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-14 11:10:58 +0100

    removed old schema column

--- src/features/iam/schema.ts
+++ src/features/iam/schema.ts
@@ -1,14 +1,10 @@
 import { pgTable, pgEnum, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const roles = pgEnum("roles", ["developer", "core", "admin"]);
-export type Roles = typeof roles;
-
 export const identities = pgTable("identities", {
   id: uuid("id").primaryKey().defaultRandom(),
   clerkId: varchar("clerk_id").notNull(),
   name: varchar("name").notNull(),
   email: varchar("email").notNull(),
-  role: roles().notNull().default("developer"),
   new_role: varchar().notNull().default("developer"),
 });
 
