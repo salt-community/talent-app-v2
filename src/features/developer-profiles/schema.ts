e221a18a30a7e34015b583ebbf5ffd94546e421e
Marcus Rådell <marcus@radell.net>
2025-02-18 09:18:36 +0100

    set developer_profiles slug to be nullable to get it to work with migrations

--- src/features/developer-profiles/schema.ts
+++ src/features/developer-profiles/schema.ts
@@ -7,7 +7,7 @@ export const developerProfiles = pgTable("developer_profiles", {
     .default(sql`gen_random_uuid()`),
   identityId: varchar("identity_id").notNull(),
   name: varchar().notNull(),
-  slug: varchar().notNull().unique(),
+  slug: varchar().unique(),
   email: varchar().notNull(),
   status: varchar("status").notNull().default("unpublished"),
 });
