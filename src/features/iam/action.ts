98b9f20c685ed23da01f9e9ae9ac75b5d507ff58
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-19 11:27:44 +0100

    removed unused file

--- src/features/iam/action.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { revalidatePath } from "next/cache";
-import { adminService } from "../admin/instance";
-
-const getIamService = async () => {
-  const { iamService } = await import("./instance");
-  return iamService;
-};
