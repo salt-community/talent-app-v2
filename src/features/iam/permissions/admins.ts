5eb39521b56ce6b7f4eaac81eee3812cebb20666
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-19 09:27:14 +0100

    fix admins permission

--- src/features/iam/permissions/admins.ts
+++ src/features/iam/permissions/admins.ts
@@ -1,15 +1,15 @@
 // TODO: Remove as the admin-dashboard is just an aggregation of the other features.
 
 export const admins = [
-  "admin.isSearchHealthOk",
-  "admin.getAllDeveloperProfiles",
-  "admin.deleteDeveloperProfile",
-  "admin.updateStatus",
-  "admin.repopulateSearch",
-  "admin.syncSearch",
-  "admin.doesSearchNeedSync",
-  "admin.getSearchSettings",
-  "admin.updateSearchSettings",
-  "admin.resetSearchSettings",
-  "admin.deleteUser",
+  "admins.isSearchHealthOk",
+  "admins.getAllDeveloperProfiles",
+  "admins.deleteDeveloperProfile",
+  "admins.updateStatus",
+  "admins.repopulateSearch",
+  "admins.syncSearch",
+  "admins.doesSearchNeedSync",
+  "admins.getSearchSettings",
+  "admins.updateSearchSettings",
+  "admins.resetSearchSettings",
+  "admins.deleteUser",
 ] as const;
