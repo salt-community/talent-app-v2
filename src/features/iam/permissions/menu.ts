5e651909954d7a4f6f0ecd6ba33f557292e3cdf0
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-19 14:13:43 +0100

    added new permissions

---
+++ src/features/iam/permissions/menu.ts
@@ -0,0 +1,15 @@
+import { Menu } from "lucide-react";
+import { PagePermissionsSchema } from "./types";
+
+type MenuViewPermission = "profile" | "admin" | "instructorsDashboard";
+
+type MenuViewPermissions = Record<MenuViewPermission, string>;
+
+export const MenuPermissions: PagePermissionsSchema<
+  "menu",
+  MenuViewPermissions
+> = {
+  profile: "menu.profile",
+  admin: "menu.admin",
+  instructorsDashboard: "menu.instructorsDashboard",
+};
