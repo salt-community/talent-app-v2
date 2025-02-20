a3bee4ae7dbd7672834914286dd8f8184b3851ba
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-20 09:33:48 +0100

    delete unused page

--- src/app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-import { developerProfilesService } from "@/features";
-import { Dashboard } from "@/features/admin";
-
-export default async function Page() {
-  const developers = await developerProfilesService.getAll();
-  return <Dashboard developers={developers} />;
-}
