88e0c479dbd9321f1519513835832e14e5c34c22
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-02-20 10:38:21 +0100

    remove unused loading.tsx

--- src/app/instructor-dashboard/cohorts/[name]/loading.tsx
+++ src/app/instructor-dashboard/cohorts/[name]/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[50vh]">
       <div className="flex flex-col items-center">
         <div className="w-10 h-10 border-4 border-red-500 border-t-transparent rounded-full animate-spin"></div>
         <p className="mt-2 text-gray-600">Loading...</p>
