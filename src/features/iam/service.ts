018fc52e77115ac69d8ca2c1e328c27429aa7de1
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-20 14:35:00 +0100

    refactored controlUser function

--- src/features/iam/service.ts
+++ src/features/iam/service.ts
@@ -3,7 +3,7 @@ import { createRepository } from "./repository";
 import { IdentityInsert } from "./schema";
 import { Db } from "@/db";
 import { auth } from "@clerk/nextjs/server";
-import { SessionClaims } from "@/features";
+import { iamService, SessionClaims } from "@/features";
 import { validateSessionClaims } from "./logic";
 import { claim } from "./session";
 import { Permission, ViewPermission } from "./permissions";
@@ -33,36 +33,25 @@ export function createService(db: Db) {
       await repository.deleteIdentity(id);
     },
     async controlUser() {
-      const SALT_DOMAIN = "appliedtechnology.se";
       const { userId, sessionClaims } = await auth();
 
-      if (!userId) {
-        return { id: "", role: "not signed in" };
-      }
+      if (!userId) return { id: "", role: "guest" };
 
       const existingUser = await repository.getUserId(userId);
+      if (existingUser) return existingUser;
 
-      if (existingUser) {
-        return existingUser;
-      }
+      return await iamService.createUserIfValid(userId, sessionClaims);
+    },
 
-      const claims = sessionClaims as SessionClaims;
+    async createUserIfValid(userId: string, sessionClaims: any) {
+      const SALT_DOMAIN = "appliedtechnology.se";
+      if (!validateSessionClaims(sessionClaims))
+        return { id: "", role: "guest" };
 
-      if (!validateSessionClaims(claims)) {
-        return;
-      }
+      const { name, email, domain } = claim(sessionClaims);
+      if (domain !== SALT_DOMAIN || !email) return { id: "", role: "guest" };
 
-      const { name, email, domain } = claim(claims);
-
-      if (domain === SALT_DOMAIN) {
-        if (!email) return;
-        const newUser = await repository.addIdentity({
-          name: name,
-          email: email,
-          clerkId: userId,
-        });
-        return newUser;
-      }
+      return await repository.addIdentity({ name, email, clerkId: userId });
     },
 
     async getCurrentUser() {
@@ -87,6 +76,7 @@ export function createService(db: Db) {
       if (!userId) return false;
       const roles: string[] = ["guest"];
       const identityRole = await repository.getIdentityRole(userId);
+      if (!identityRole) return false;
       roles.push(identityRole.roles);
       return hasAccess(roles, permission);
     },
