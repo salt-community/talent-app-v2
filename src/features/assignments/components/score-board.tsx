c8d00c6e8e7b88897a94612884f920d3153aab08
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-21 11:58:17 +0100

    include spider graph and remove old function

--- src/features/assignments/components/score-board.tsx
+++ src/features/assignments/components/score-board.tsx
@@ -1,25 +1,20 @@
 import { Separator, H2 } from "@/components/ui";
 import { AverageScore } from "./average-score";
 import { assignmentsService } from "@/features";
+import { SpiderGraph } from "./spider-graph";
 
 type ScoreBoardProps = {
   identityId: string;
 };
 
 export async function ScoreBoard({ identityId }: ScoreBoardProps) {
-  const getCohortIdentity =
-    await assignmentsService.getCohortIdentity(identityId);
-
-  // const assignments = await assignmentsService.getAssignmentsByCohortId(
-  //   getCohortIdentity.cohortId
-  // );
   return (
     <section className="min-w-72">
       <Separator className="my-4" />
       <H2>Salt Scoring</H2>
 
       <AverageScore identityId={identityId} />
-      {/* <SpiderGraph assignments={assignments} /> */}
+      <SpiderGraph identityId={identityId} />
     </section>
   );
 }
