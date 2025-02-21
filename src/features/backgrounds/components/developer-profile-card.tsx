35a31e8cf92a03b0ad3a43838fedd488994b25ee
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-21 09:28:58 +0100

    style developer profile card

--- src/features/backgrounds/components/developer-profile-card.tsx
+++ src/features/backgrounds/components/developer-profile-card.tsx
@@ -11,7 +11,7 @@ export function DeveloperProfileCard({ developerProfileId }: Props) {
     <section className="px-4 container mx-auto">
       <ul className="pb-10 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
         <li className="">
-          <Card className="p-2 h-full justify-center min-w-[20rem]">
+          <Card className="p-2 h-full justify-center min-w-[22rem] cursor-pointer hover:shadow-lg transition-shadow">
             <CardContent>
               <Background
                 developerProfileId={developerProfileId}
