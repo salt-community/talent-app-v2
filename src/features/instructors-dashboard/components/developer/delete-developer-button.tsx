01fa51d888c0908dc3d68bb1cd87d9cdaf17e490
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-02-21 11:53:20 +0100

    add alert dialog when deleting a developer form a cohort

--- src/features/instructors-dashboard/components/developer/delete-developer-button.tsx
+++ src/features/instructors-dashboard/components/developer/delete-developer-button.tsx
@@ -1,6 +1,7 @@
 import { TrashIcon } from "lucide-react";
 import React from "react";
 import { deleteIdentityFromCohortAction } from "../../action";
+import { AlertDialogDemo } from "@/components";
 
 export function DeleteDeveloperButton({ identityId }: { identityId: string }) {
   async function handleDelete() {
@@ -8,13 +9,18 @@ export function DeleteDeveloperButton({ identityId }: { identityId: string }) {
   }
   return (
     <div>
-      <button
-        className="text-red-500 hover:bg-gray-100 p-1.5 rounded-md transition-colors"
-        aria-label="Delete"
-        onClick={handleDelete}
+      <AlertDialogDemo
+        title="Are you sure?"
+        description="This action can't be undone, are you sure?"
+        onConfirm={handleDelete}
       >
-        <TrashIcon size={18} />
-      </button>
+        <button
+          className="text-red-500 hover:bg-gray-100 p-1.5 rounded-md transition-colors"
+          aria-label="Delete"
+        >
+          <TrashIcon size={18} />
+        </button>
+      </AlertDialogDemo>
     </div>
   );
 }
