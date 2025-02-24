ae3e43b9c8b3f4b0da033182f599be17b12bcc0f
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-24 13:19:45 +0100

    removed delayed developerProfileService init

--- src/features/backgrounds/instance.ts
+++ src/features/backgrounds/instance.ts
@@ -4,8 +4,6 @@ import { getDeveloperProfilesService, secureService } from "@/features";
 import { createMeiliClient } from "./meili";
 import { db } from "@/db";
 
-const developerProfilesService = getDeveloperProfilesService();
-
 export const insecureBackgroundsService = createBackgroundsService(
   createRepository(db),
   createMeiliClient()
