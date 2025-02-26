8509ff12e7b5d872602b924ea4e65f0366822236
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-26 13:48:35 +0100

    updated name of schema file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: ["./src/features/*/schema.ts", "./src/features/**/*/schema.ts"],
+  schema: [
+    "./src/features/*/schema.ts",
+    "./src/features/**/*/schema.ts",
+    "./src/features/*/db-schema.ts",
+  ],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
