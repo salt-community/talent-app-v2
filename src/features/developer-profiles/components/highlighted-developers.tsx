09b6fdadb56286d73a19645a828e12990ebf33ec
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-02-26 11:48:30 +0100

    comment out clickability on highlighted developers

--- src/features/developer-profiles/components/highlighted-developers.tsx
+++ src/features/developer-profiles/components/highlighted-developers.tsx
@@ -1,28 +1,27 @@
 import { Card, CardContent } from "@/components";
-import { DeveloperProfileLink } from "./developer-profile-link";
+// import { DeveloperProfileLink } from "./developer-profile-link";
 import { Background } from "../backgrounds";
 
 type Props = {
   developerProfileIds: string[];
 };
 
+//commented out clickability
+
 export async function HighlightedDevelopers({ developerProfileIds }: Props) {
   return (
     <>
       <ul className="px-4 pt-10 grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
         {developerProfileIds.map((developerProfileId, index) => (
-          <li key={index} className="">
-            <Card className="p-2 h-full flex justify-center min-w-[20rem]">
+            <Card key={index} className="p-2 h-full flex justify-center min-w-[20rem]">
               <CardContent>
-                <DeveloperProfileLink developerProfileId={developerProfileId}>
+                {/* <DeveloperProfileLink developerProfileId={developerProfileId}> */}
                   <Background
                     developerProfileId={developerProfileId}
-                    page="highlight"
                   />
-                </DeveloperProfileLink>
+                {/* </DeveloperProfileLink> */}
               </CardContent>
             </Card>
-          </li>
         ))}
       </ul>
     </>
