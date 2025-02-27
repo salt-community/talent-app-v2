066b58a7e034bd567e11a3ca5f0caa4f0775a405
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-27 10:49:45 +0100

    renamed tables and updated repository functions

--- src/features/developer-profiles/db-schema.ts
+++ src/features/developer-profiles/db-schema.ts
@@ -12,38 +12,51 @@ export const developerProfiles = pgTable("developer_profiles", {
   email: varchar().notNull(),
   status: varchar("status").notNull().default("unpublished"),
 });
-export const backgrounds = pgTable("backgrounds", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  developerProfileId: uuid("developer_profile_id").notNull(),
-  avatarUrl: varchar("avatar_url").notNull().default(""),
-  name: varchar().notNull(),
-  title: varchar().notNull(),
-  bio: varchar().notNull(),
-  links: jsonb().$type<SocialLink[]>().notNull(),
-});
-export const skills = pgTable("background_skills", {
+export const developerProfileBackgrounds = pgTable(
+  "developer_profiles_backgrounds",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    developerProfileId: uuid("developer_profile_id").notNull(),
+    avatarUrl: varchar("avatar_url").notNull().default(""),
+    name: varchar().notNull(),
+    title: varchar().notNull(),
+    bio: varchar().notNull(),
+    links: jsonb().$type<SocialLink[]>().notNull(),
+  }
+);
+export const developerProfileSkills = pgTable("developer_profiles_skills", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   backgroundId: integer("background_id")
     .notNull()
-    .references(() => backgrounds.id, { onDelete: "cascade" }),
+    .references(() => developerProfileBackgrounds.id, { onDelete: "cascade" }),
   name: varchar().notNull(),
   level: integer().notNull().default(5),
 });
-export const educations = pgTable("background_educations", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  backgroundId: integer("background_id")
-    .notNull()
-    .references(() => backgrounds.id, { onDelete: "cascade" }),
-  name: varchar().notNull(),
-});
-export const languages = pgTable("background_languages", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  backgroundId: integer("background_id")
-    .notNull()
-    .references(() => backgrounds.id, { onDelete: "cascade" }),
-  name: varchar().notNull(),
-  level: integer().notNull().default(5),
-});
+export const developerProfileEducations = pgTable(
+  "developer_profiles_educations",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    backgroundId: integer("background_id")
+      .notNull()
+      .references(() => developerProfileBackgrounds.id, {
+        onDelete: "cascade",
+      }),
+    name: varchar().notNull(),
+  }
+);
+export const developerProfileLanguages = pgTable(
+  "developer_profiles_languages",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    backgroundId: integer("background_id")
+      .notNull()
+      .references(() => developerProfileBackgrounds.id, {
+        onDelete: "cascade",
+      }),
+    name: varchar().notNull(),
+    level: integer().notNull().default(5),
+  }
+);
 export const meiliSearchOutbox = pgTable("meili_search_outbox", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   developerProfileId: uuid("developer_profile_id").notNull(),
