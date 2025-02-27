2f469438406ab2809f75f9a2debd8c8ae2790569
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-02-27 09:20:38 +0100

    refactor: enhance grid layout for highlighted developers component

--- src/features/developer-profiles/components/highlighted-developers.tsx
+++ src/features/developer-profiles/components/highlighted-developers.tsx
@@ -11,7 +11,7 @@ type Props = {
 export async function HighlightedDevelopers({ developerProfileIds }: Props) {
   return (
     <>
-      <ul className="px-4 pt-10 grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+      <ul className="px-4 pt-10 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4" >
         {developerProfileIds.map((developerProfileId, index) => (
             <Card key={index} className="p-2 h-full flex justify-center min-w-[20rem]">
               <CardContent>
