3549bdeda4cc6204cb96b7985ddca803c82bad48
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-02-27 11:13:28 +0100

    refactor: replace button with Button component in DeleteAssignmentButton for consistency

--- src/features/instructors-dashboard/components/assignments/delete-assignment-button.tsx
+++ src/features/instructors-dashboard/components/assignments/delete-assignment-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { TrashIcon } from "lucide-react";
 import React from "react";
-import { AlertDialogDemo } from "@/components";
+import { AlertDialogDemo, Button } from "@/components";
 import { useToast } from "@/hooks/use-toast";
 import { deleteAssignmentByIdAction } from "../../action";
 
@@ -32,12 +32,13 @@ export function DeleteAssignmentButton({ assignmentId, name }: Props) {
         description="This action can't be undone, are you sure?"
         onConfirm={handleDelete}
       >
-        <button
-          className="text-red-500 hover:bg-gray-100 p-1.5 rounded-md transition-colors"
+        <Button
+        variant="ghost"
+          className="hover:text-destructive"
           aria-label="Delete"
         >
           <TrashIcon size={18} />
-        </button>
+        </Button>
       </AlertDialogDemo>
     </div>
   );
