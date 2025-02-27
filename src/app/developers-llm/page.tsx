95a838124c9cdd49af6e58f922f4a1168b000f50
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-02-27 14:36:49 +0100

    feat: display ranking with tooltip with more info

--- src/app/developers-llm/page.tsx
+++ src/app/developers-llm/page.tsx
@@ -2,6 +2,13 @@ import { errorHandler } from "@/lib";
 import { developerProfilesService } from "@/features/developer-profiles";
 import { Search } from "../developers/search";
 import { DeveloperCard } from "@/features/developer-profiles/components/developer-card";
+import { Card } from "@/components";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 type Props = { searchParams: Promise<{ search: string | undefined }> };
 
@@ -18,16 +25,28 @@ export default async function Page({ searchParams }: Props) {
   return (
     <main className="px-4 pb-6">
       <Search />
-      <ul className="pt-20 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+      <ul className="pt-20 grid gap-x-4 gap-y-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
         {developerProfiles.map((developerProfile, index) => (
-          <li key={index} className="">
-            <div className="place-self-end">
-              Ranking: {developerProfile.ranking}%
-            </div>
-            <DeveloperCard
-              key={index}
-              developerProfileId={developerProfile.id}
-            />
+          <li key={index} className="relative">
+            <DeveloperCard developerProfileId={developerProfile.id} />
+            {developerProfile.ranking && (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger
+                    className="absolute top-[-5] right-[-5]
+                  bg-lightGray text-xs text-white rounded-full p-2"
+                  >
+                    {developerProfile.ranking}%
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className="text-sm text-center">
+                      This developer profile matches your search by{" "}
+                      {developerProfile.ranking}%.
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
           </li>
         ))}
       </ul>
