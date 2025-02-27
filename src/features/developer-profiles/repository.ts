eaf6202760c991abba89bc97b0d87e8d58c7d284
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-27 09:26:59 +0100

    moved and renamed update to updateBackground

--- src/features/developer-profiles/repository.ts
+++ src/features/developer-profiles/repository.ts
@@ -10,6 +10,7 @@ import {
 } from "./db-schema";
 import {
   BackgroundInsert,
+  BackgroundUpdate,
   DeveloperProfileInsert,
   EducationSelect,
   LanguageSelect,
@@ -241,6 +242,57 @@ export function createDevelopersRepository(db: Db) {
         }
       );
       return { outboxMessageId, backgroundId };
+    },
+    async updateBackground(background: BackgroundUpdate) {
+      const outboxMessageId = await db.transaction(async (tx) => {
+        // TODO: Don't use the primary key at all in this function.
+        if (background.id === -1) {
+          await tx.insert(backgrounds).values({
+            developerProfileId: background.developerProfileId,
+            bio: background.bio || "",
+            name: background.name || "",
+            title: background.title || "",
+            avatarUrl: background.avatarUrl || "",
+            links: background.links || [],
+          });
+        } else {
+          const { id: backgroundId, ...rest } = background;
+          await tx
+            .update(backgrounds)
+            .set({ ...rest })
+            .where(eq(backgrounds.id, backgroundId));
+
+          await tx.delete(skills).where(eq(skills.backgroundId, backgroundId));
+          for (const skill of background.skills) {
+            await tx.insert(skills).values({ backgroundId, name: skill });
+          }
+          await tx
+            .delete(languages)
+            .where(eq(languages.backgroundId, backgroundId));
+          for (const language of background.languages) {
+            await tx.insert(languages).values({ backgroundId, name: language });
+          }
+          await tx
+            .delete(educations)
+            .where(eq(educations.backgroundId, backgroundId));
+          for (const education of background.educations) {
+            await tx
+              .insert(educations)
+              .values({ backgroundId, name: education });
+          }
+        }
+
+        return (
+          await tx
+            .insert(meiliSearchOutbox)
+            .values({
+              developerProfileId: background.developerProfileId,
+              operation: "upsert",
+            })
+            .returning({ id: meiliSearchOutbox.id })
+        )[0].id;
+      });
+      return { outboxMessageId };
     },
   };
 }
