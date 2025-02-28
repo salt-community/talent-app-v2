ccee409a19454f391689e513c127bc8b418ed909
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-28 13:56:01 +0100

    new migration file

--- drizzle/0007_next_robin_chapel.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "developer_profiles" ADD COLUMN "avatar_url" varchar DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE "developer_profiles" ADD COLUMN "title" varchar NOT NULL;--> statement-breakpoint
-ALTER TABLE "developer_profiles" ADD COLUMN "bio" varchar NOT NULL;--> statement-breakpoint
-ALTER TABLE "developer_profiles" ADD COLUMN "links" jsonb NOT NULL;
