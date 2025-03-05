963612071958a69d346e7fe93e9a43fd93f5a28a
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-03-05 11:37:28 +0100

    enhance BackgroundAvatar component to support large size option

--- src/features/developer-profiles/components/backgrounds/avatar.tsx
+++ src/features/developer-profiles/components/backgrounds/avatar.tsx
@@ -2,13 +2,17 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/";
 
 type Props = {
   url: string;
-  size?: "sm";
+  size?: "sm" | "lg";
 };
 
 const DEFAULT_AVATAR = "/avatar.png";
 export function BackgroundAvatar({ url, size }: Props) {
+
+ const width = size === "sm" ? "w-12" : size === "lg" ? "w-52" : "";
+
+  
   return (
-    <Avatar className={size ? "w-12 h-auto" : ""}>
+    <Avatar className={size ? `${width} h-auto` : ""}>
       <AvatarImage src={url === "" ? DEFAULT_AVATAR : url} />
       <AvatarFallback className="text-xs">Wrong URL</AvatarFallback>
     </Avatar>
