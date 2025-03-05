c4a5dd3a199db7bb187809ee8827e7813af58e97
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-03-05 12:23:30 +0100

    enhance CvHeader layout for better responsiveness and alignment

--- src/features/developer-profiles/components/cv/cv-header.tsx
+++ src/features/developer-profiles/components/cv/cv-header.tsx
@@ -8,12 +8,15 @@ type Props = {
  };
 export function CvHeader({ name, introduction, avatarUrl } : Props) {
   return (
-    <article className="flex flex-col items-center space-y-4">
-      <BackgroundAvatar url={avatarUrl} size="lg" />
-      <div className="flex flex-col items-center space-y-4 px-3">
+    <article className="flex flex-col items-center md:items-start justify-center gap-4 md:gap-0 md:grid md:grid-cols-[1fr 2fr]">
+      <div className="md:col-start-1 md:col-end-2 px-4 h-full">
+        <BackgroundAvatar url={avatarUrl} size="lg" />
+      </div>
+      <div className="flex flex-col items-center gap-4 px-3 md:items-start md:col-start-2 md:col-end-3">
         <h1 className="text-3xl font-bold capitalize">{name}</h1>
-        <p className="font-light text-paragraph text-center">
-          {introduction + `I am a self-taught full-stack JavaScript developer
+        <p className="font-light text-paragraph text-center md:text-left">
+          {introduction +
+            `I am a self-taught full-stack JavaScript developer
           with a passion for building user-friendly and accessible applications.
           With a background in special education, I bring a unique perspective
           to digital accessibility. I have experience with Next.js, React,
