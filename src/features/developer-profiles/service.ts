48d7ec2f4a6805030f3c611c81a86f5689b43b95
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-05 11:47:41 +0100

    restructured service functions

--- src/features/developer-profiles/service.ts
+++ src/features/developer-profiles/service.ts
@@ -98,11 +98,81 @@ export function createDeveloperProfilesService(
     async getAllById(identityId: string) {
       return await repository.getAllById(identityId);
     },
+    async getHighlightedDeveloperProfileIds() {
+      const highlighted = await repository.getAll();
+      return highlighted
+        .filter((dev) => dev.status === "highlighted")
+        .map((dev) => dev.id);
+    },
+    async getDeveloperProfileByIdentityId(identityId: string) {
+      return await repository.getDeveloperProfileByIdentityId(identityId);
+    },
+    async getCurrentUsers() {
+      const currentUser = await getCurrentUser();
+      if (!currentUser) {
+        return;
+      }
+
+      const developerProfile = await this.getDeveloperProfileByIdentityId(
+        currentUser.id
+      );
+      const user = {
+        ...currentUser,
+        developerProfile,
+      };
+      return user;
+    },
+    async getBackgroundByDeveloperProfileId(developerProfileId: string) {
+      const [background] =
+        await repository.getBackgroundById(developerProfileId);
+
+      type T = typeof background;
+
+      if (!background) {
+        return {
+          id: "",
+          avatarUrl: "",
+          name: "<New Profile>",
+          identityId: "",
+          title: "",
+          bio: "",
+          links: [],
+          skills: [],
+          languages: [],
+          educations: [],
+        } as T;
+      }
+      return background;
+    },
+    async getAllSkills() {
+      return (await repository.getAllSkills()).filter(
+        (skill, index, array) =>
+          array.findIndex((s) => s.name === skill.name) === index
+      );
+    },
+    async getAllLanguages() {
+      return (await repository.getAllLanguages()).filter(
+        (language, index, array) =>
+          array.findIndex((l) => l.name === language.name) === index
+      );
+    },
+    async getAllEducations() {
+      return (await repository.getAllEducations()).filter(
+        (education, index, array) =>
+          array.findIndex((e) => e.name === education.name) === index
+      );
+    },
     async delete(id: string) {
       await repository.deleteTempDeveloperProfile(id);
     },
     async deleteByIdentityId(identityId: string) {
       await repository.deleteTempDeveloperProfileByIdentityId(identityId);
+    },
+    async deleteBackgroundById(developerProfileId: string) {
+      await repository.deleteBackgroundById(developerProfileId);
+    },
+    async deleteMeiliSearchDocument(developerProfileId: string) {
+      await backgroundsSearchApi.deleteDocument(developerProfileId);
     },
     async updateStatus(args: { id: string; status: string }) {
       const developerProfile = {
@@ -110,6 +180,64 @@ export function createDeveloperProfilesService(
         status: args.status,
       };
       await repository.updateTempDeveloperProfile(developerProfile);
+    },
+    async updateMissingSlugs() {
+      const developers = await repository.getAll();
+      for (const developer of developers) {
+        if (!developer.slug) {
+          const newSlug = generateSlug(developer.name);
+          await repository.updateTempDeveloperProfile({
+            id: developer.id,
+            slug: newSlug,
+          });
+        }
+      }
+    },
+    async updateDeveloperProfile(
+      developerProfileUpdates: updateTempDeveloperProfile
+    ) {
+      const { outboxMessageId } = await repository.updateTempDeveloperProfile(
+        developerProfileUpdates
+      );
+      console.log("updates:", developerProfileUpdates);
+      const developerProfile =
+        await repository.getBackgroundByDeveloperProfileId(
+          developerProfileUpdates.id
+        );
+
+      const status = await backgroundsSearchApi.upsertDocuments([
+        developerProfile[0],
+      ]);
+      if (OK_STATUSES.includes(status)) {
+        await repository.removeOutboxMessage(outboxMessageId);
+      }
+    },
+    async generateUniqueSlug(name: string) {
+      const slug = generateSlug(name);
+      let uniqueSlug = slug;
+      let count = 1;
+
+      while (await repository.existsBySlug(uniqueSlug)) {
+        uniqueSlug = `${slug}-${count}`;
+        count++;
+      }
+
+      return uniqueSlug;
+    },
+    async syncMeilisearch() {
+      const outboxMessages = await repository.getAllOutboxMessage();
+      for (const outboxMessage of outboxMessages) {
+        updateMeilisearchFor(outboxMessage);
+      }
+    },
+    async repopulateMeiliSearch() {
+      await backgroundsSearchApi.deleteIndex();
+      await backgroundsSearchApi.ensureIndex();
+      const backgrounds = await repository.getAllBackgrounds();
+      await backgroundsSearchApi.upsertDocuments(backgrounds);
+    },
+    async doesMeilisearchNeedSync() {
+      return (await repository.getAllOutboxMessage()).length > 0;
     },
     async createDeveloperProfile(identityId: string) {
       const { sessionClaims } = await auth();
@@ -135,134 +263,8 @@ export function createDeveloperProfilesService(
 
       return { id: developerProfileId };
     },
-
-    async generateUniqueSlug(name: string) {
-      const slug = generateSlug(name);
-      let uniqueSlug = slug;
-      let count = 1;
-
-      while (await repository.existsBySlug(uniqueSlug)) {
-        uniqueSlug = `${slug}-${count}`;
-        count++;
-      }
-
-      return uniqueSlug;
-    },
-    async getDeveloperProfileByIdentityId(identityId: string) {
-      return await repository.getDeveloperProfileByIdentityId(identityId);
-    },
-    async getCurrentUsers() {
-      const currentUser = await getCurrentUser();
-      if (!currentUser) {
-        return;
-      }
-
-      const developerProfile = await this.getDeveloperProfileByIdentityId(
-        currentUser.id
-      );
-      const user = {
-        ...currentUser,
-        developerProfile,
-      };
-      return user;
-    },
-    async updateMissingSlugs() {
-      const developers = await repository.getAll();
-      for (const developer of developers) {
-        if (!developer.slug) {
-          const newSlug = generateSlug(developer.name);
-          await repository.updateTempDeveloperProfile({
-            id: developer.id,
-            slug: newSlug,
-          });
-        }
-      }
-    },
-    async syncMeilisearch() {
-      const outboxMessages = await repository.getAllOutboxMessage();
-      for (const outboxMessage of outboxMessages) {
-        updateMeilisearchFor(outboxMessage);
-      }
-    },
-    async getBackgroundByDeveloperProfileId(developerProfileId: string) {
-      const [background] =
-        await repository.getBackgroundById(developerProfileId);
-
-      type T = typeof background;
-
-      if (!background) {
-        return {
-          id: "",
-          avatarUrl: "",
-          name: "<New Profile>",
-          identityId: "",
-          title: "",
-          bio: "",
-          links: [],
-          skills: [],
-          languages: [],
-          educations: [],
-        } as T;
-      }
-
-      return background;
-    },
-    async getAllSkills() {
-      return (await repository.getAllSkills()).filter(
-        (skill, index, array) =>
-          array.findIndex((s) => s.name === skill.name) === index
-      );
-    },
-    async getAllLanguages() {
-      return (await repository.getAllLanguages()).filter(
-        (language, index, array) =>
-          array.findIndex((l) => l.name === language.name) === index
-      );
-    },
-    async getAllEducations() {
-      return (await repository.getAllEducations()).filter(
-        (education, index, array) =>
-          array.findIndex((e) => e.name === education.name) === index
-      );
-    },
     async addDeveloperProfileDetails(background: BackgroundInsert) {
       await repository.addDeveloperProfileDetails(background);
-    },
-    async updateDeveloperProfile(
-      developerProfileUpdates: updateTempDeveloperProfile
-    ) {
-      const { outboxMessageId } = await repository.updateTempDeveloperProfile(
-        developerProfileUpdates
-      );
-      console.log("updates:", developerProfileUpdates);
-      const developerProfile =
-        await repository.getBackgroundByDeveloperProfileId(
-          developerProfileUpdates.id
-        );
-
-      const status = await backgroundsSearchApi.upsertDocuments([
-        developerProfile[0],
-      ]);
-      if (OK_STATUSES.includes(status)) {
-        await repository.removeOutboxMessage(outboxMessageId);
-      }
-    },
-    async repopulateMeiliSearch() {
-      await backgroundsSearchApi.deleteIndex();
-      await backgroundsSearchApi.ensureIndex();
-
-      const backgrounds = await repository.getAllBackgrounds();
-
-      await backgroundsSearchApi.upsertDocuments(backgrounds);
-    },
-    async doesMeilisearchNeedSync() {
-      return (await repository.getAllOutboxMessage()).length > 0;
-    },
-    async deleteBackgroundById(developerProfileId: string) {
-      await repository.deleteBackgroundById(developerProfileId);
-    },
-    async deleteMeiliSearchDocument(developerProfileId: string) {
-      await backgroundsSearchApi.deleteDocument(developerProfileId);
     },
     async addTempDeveloperProfile(args: {
       developerProfile: DeveloperProfileInsert;
@@ -272,12 +274,6 @@ export function createDeveloperProfilesService(
         args.developerProfile,
         args.backgrounds
       );
-    },
-    async getHighlightedDeveloperProfileIds() {
-      const highlighted = await repository.getAll();
-      return highlighted
-        .filter((dev) => dev.status === "highlighted")
-        .map((dev) => dev.id);
     },
   };
 }
