125a7ae829d1163f4eec301ed4784d11d982948c
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-03-05 12:36:16 +0100

    add footer element to CvContainer for enhanced visual structure

--- src/features/developer-profiles/components/cv/cv-container.tsx
+++ src/features/developer-profiles/components/cv/cv-container.tsx
@@ -17,6 +17,7 @@ export async function CvContainer({
 
   return (
     <section className="border-red-800 border-2 py-6 md:py-0 md:mx-auto md:min-h-[1122px] md:max-h-[1122px]  md:min-w-[795px] md:max-w-[795px]">
+     <div className="hidden md:block text-end py-3 pr-4 bg-zinc-100 font-bold">{"</salt>"}</div>
       <CvHeader
         name={background.name}
         introduction={background.bio}
