4e37ed3f98fc97e7d1170413aa9d281ac6f7b42c
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-06 13:19:47 +0100

    migration scripts to remove column backgroundId

---
+++ drizzle/0012_unknown_radioactive_man.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "developer_profiles_educations" DROP COLUMN IF EXISTS "background_id";--> statement-breakpoint
+ALTER TABLE "developer_profiles_languages" DROP COLUMN IF EXISTS "background_id";--> statement-breakpoint
+ALTER TABLE "developer_profiles_skills" DROP COLUMN IF EXISTS "background_id";
