6e89c0baaa5868229c74a9901be2943ac757ec66
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-03-06 14:11:36 +0100

    feat: enhance developer assignments display with improved layout and linking

--- src/features/developer-profiles/components/developer-assigments.tsx
+++ src/features/developer-profiles/components/developer-assigments.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { developerProfilesService } from "../instance";
+import { Separator } from "@/components";
 
 type Props = {
   identityId: string;
@@ -9,22 +11,29 @@ export async function DeveloperAssignments({ identityId }: Props) {
     await developerProfilesService.getScoredAssignmentsByIdentityId(identityId);
 
   return (
-    <div>
-      <div>
-        {assignments.length === 0 ? (
-          <div>No assignments</div>
-        ) : (
-          assignments.map((assignment) => (
-            <div key={assignment.id}>
-              {
-                <div>
-                  <div>{assignment.title}</div>
-                  <div>{assignment?.scores?.status}</div>
-                </div>
-              }
+    <div className="max-w-6xl mx-auto p-4">
+      <div className="flex justify-between items-center py-2">
+        <h2 className="text-2xl font-semibold">Assignments</h2>
+      </div>
+      <Separator className="mb-4" />
+      <div className="space-y-4">
+        {assignments.map((assignment) => {
+          return (
+            <div
+              key={assignment.id}
+              className="flex justify-between items-center border-b pb-4"
+            >
+              <div className="flex items-center gap-4">
+                <Link
+                  href={`/}`}
+                  className="text-header font-medium hover:underline hover:underline-offset-4"
+                >
+                  {assignment.title}
+                </Link>
+              </div>
             </div>
-          ))
-        )}
+          );
+        })}
       </div>
     </div>
   );
