6de8c43fc2b14058911702835b5f4614e1e09b57
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-03-10 12:40:24 +0100

    integrate updateCvAction for saving background information in CV component

--- src/features/developer-profiles/components/cv/cv-container.tsx
+++ src/features/developer-profiles/components/cv/cv-container.tsx
@@ -5,6 +5,7 @@ import { CvAside } from "./cv-aside";
 import { CvHeader } from "./cv-header";
 import { BackgroundInfo } from "../../types";
 import { Check, Pencil } from "lucide-react";
+import { updateCvAction } from "../../actions";
 
 type Props = {
   background: BackgroundInfo;
@@ -15,9 +16,9 @@ export function CvContainer({ background, hasProfileAccess }: Props) {
   const [isEditable, setIsEditable] = useState(false);
   const [backgroundInfo, setBackgroundInfo] = useState(background);
   const handleOnSave = async (background: BackgroundInfo) => {
-    console.log(background)
-    setIsEditable(false)
-  }
+    await updateCvAction(background)
+    setIsEditable(false);
+  };
 
   return (
     <section className="border-red-800 border-2 py-6 md:py-0 md:mx-auto md:min-h-[1122px] md:max-h-[1122px]  md:min-w-[795px] md:max-w-[795px]">
