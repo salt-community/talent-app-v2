653f42f19ed49cf6c7ba1aa7d863644ce35a147e
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-03-11 11:21:30 +0100

    feat: add functionality to manage education and work experience entries in CV

--- src/features/developer-profiles/components/cv/cv-block.tsx
+++ src/features/developer-profiles/components/cv/cv-block.tsx
@@ -1,5 +1,6 @@
-import { Edit } from "lucide-react";
+import { Edit, X } from "lucide-react";
 import { EditableField } from "./editable-field";
+import { Button } from "@/components";
 
 type Props = {
   school: string;
@@ -7,9 +8,17 @@ type Props = {
   title: string;
   description: string;
   isEditable: boolean;
+  onDelete: () => void;
 };
 
-export function CvBlock({ school, date, title, description, isEditable }: Props) {
+export function CvBlock({
+  school,
+  date,
+  title,
+  description,
+  isEditable,
+  onDelete,
+}: Props) {
   return (
     <div>
       <div className="flex items-center gap-2">
@@ -28,6 +37,11 @@ export function CvBlock({ school, date, title, description, isEditable }: Props)
             console.log(value);
           }}
         />
+        {isEditable && (
+          <Button variant="link" size="icon" onClick={onDelete}>
+            <X size={24} className="cursor-pointer" />
+          </Button>
+        )}
       </div>
       <EditableField
         value={date}
--- src/features/developer-profiles/components/cv/cv-main-content.tsx
+++ src/features/developer-profiles/components/cv/cv-main-content.tsx
@@ -1,20 +1,107 @@
-import { H2 } from "@/components";
+import { Button, H2 } from "@/components";
 import { CvBlock } from "./cv-block";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
 type Props = {
   isEditable: boolean;
 };
 export function CvMainContent({ isEditable }: Props) {
+  const [educations, setEducations] = useState([
+    {
+      id: 1,
+      school: "University of Technology",
+      date: "2020 - 2024",
+      title: "Bachelor of Science in Computer Science",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ]);
+  const [workExperiences, setWorkExperiences] = useState([
+    {
+      id: 1,
+      company: "Tech Company",
+      date: "2022 - Present",
+      title: "Software Engineer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ]);
+
   return (
     <div>
-      <H2>Education and training</H2>
-      <CvBlock
-        school="University of Technology"
-        date="2020 - 2024"
-        title="Bachelor of Science in Computer Science"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        isEditable={isEditable}
-      />
+      <div className="flex justify-between">
+        <H2>Education and training</H2>
+        <Button
+          onClick={() => {
+            setEducations((prev) => [
+              ...prev,
+              {
+                id: prev.length + 1,
+                school: "School name",
+                date: "xxxx - xxxx",
+                title: "Education title",
+                description: "Education description",
+              },
+            ]);
+          }}
+        >
+          <Plus />
+        </Button>
+      </div>
+      <div className="flex flex-col gap-4">
+        {educations.map((education) => (
+          <CvBlock
+            key={education.id}
+            school={education.school}
+            date={education.date}
+            title={education.title}
+            description={education.description}
+            isEditable={isEditable}
+            onDelete={() => {
+              setEducations((prev) =>
+                prev.filter((e) => e.id !== education.id),
+              );
+            }}
+          />
+        ))}
+      </div>
+      <div className="flex justify-between">
+        <H2>Work experience</H2>
+        <Button
+          onClick={() => {
+            setWorkExperiences((prev) => [
+              ...prev,
+              {
+                id: prev.length + 1,
+                company: "Company name",
+                date: "xxxx - xxxx",
+                title: "Job title",
+                description: "Job description",
+              },
+            ]);
+          }}
+        >
+          <Plus />
+        </Button>
+      </div>
+      <div className="flex flex-col gap-4">
+        {workExperiences.map((experience) => (
+          <CvBlock
+            key={experience.id}
+            school={experience.company}
+            date={experience.date}
+            title={experience.title}
+            description={experience.description}
+            isEditable={isEditable}
+            onDelete={() => {
+              setWorkExperiences((prev) =>
+                prev.filter((e) => e.id !== experience.id),
+              );
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
