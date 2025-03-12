622065c6b75eeedb5ea4801e543c22bba4b36373
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-12 11:34:21 +0100

    removed unused import

--- src/features/assignments/actions.ts
+++ src/features/assignments/actions.ts
@@ -1,6 +1,5 @@
 "use server";
 import { revalidatePath } from "next/cache";
-import { ZodError } from "zod";
 import { errorHandler } from "@/lib";
 import { Assignment } from "./types";
 import { assignmentsService } from "./instance";
