f548d6e71acb3a6ace0ce485fd77b737fee49a20
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-12 11:19:25 +0100

    removed old types

--- src/features/developer-profiles/types.ts
+++ src/features/developer-profiles/types.ts
@@ -11,20 +11,11 @@ import { JwtPayload } from "jsonwebtoken";
 export type DevelopersService = ReturnType<
   typeof createDeveloperProfilesService
 >;
-
-export type Developer = {
-  name: string;
-  id: string;
-  identityId: string | null;
-  email: string;
-  status: string;
-};
 export interface SessionClaims extends JwtPayload {
   first_name?: string;
   last_name?: string;
   email?: string | undefined;
 }
-export type DeveloperProfileInsert = typeof tempDeveloperProfiles.$inferInsert;
 
 export type updateDeveloperProfile = {
   id: string;
@@ -93,16 +84,6 @@ export type PreviousState =
     }
   | undefined;
 
-export type typeBackground = {
-  id: number;
-  developerProfileId: string;
-  name: string;
-  avatarUrl: string;
-  title: string;
-  bio: string;
-  links: SocialLink[];
-};
-
 export type typeDeveloperProfile = {
   id: string;
   identityId: string;
@@ -121,9 +102,6 @@ export type BackgroundForDeveloperProfile = {
   links?: SocialLink[];
 };
 
-export type OutboxMessageInsert = typeof meiliSearchOutbox.$inferInsert;
-export type OutboxMessageSelect = typeof meiliSearchOutbox.$inferSelect;
-
 export type developerProfileDetails = {
   developerProfileId: string;
   skills: string[];
@@ -132,14 +110,18 @@ export type developerProfileDetails = {
   links?: SocialLink[];
 };
 
+export type SocialLink = {
+  url: string;
+  name: string;
+};
+
+export type DeveloperProfileInsert = typeof tempDeveloperProfiles.$inferInsert;
+export type OutboxMessageInsert = typeof meiliSearchOutbox.$inferInsert;
+export type OutboxMessageSelect = typeof meiliSearchOutbox.$inferSelect;
+
 export type SkillInsert = typeof developerProfileSkills.$inferInsert;
 export type SkillSelect = typeof developerProfileSkills.$inferSelect;
 export type LanguageInsert = typeof developerProfileLanguages.$inferInsert;
 export type LanguageSelect = typeof developerProfileLanguages.$inferSelect;
 export type EducationInsert = typeof developerProfileEducations.$inferInsert;
 export type EducationSelect = typeof developerProfileEducations.$inferSelect;
-
-export type SocialLink = {
-  url: string;
-  name: string;
-};
