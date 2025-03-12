4d917228c00b7411f53ec14ce9460fcd043f6a89
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-12 15:15:31 +0100

    removed temp table from schema

--- src/features/developer-profiles/db-schema.ts
+++ src/features/developer-profiles/db-schema.ts
@@ -44,20 +44,6 @@ export const meiliSearchOutbox = pgTable("meili_search_outbox", {
   operation: varchar().notNull(),
 });
 
-export const tempDeveloperProfiles = pgTable("temp_developer_profiles", {
-  id: uuid()
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  identityId: varchar("identity_id").notNull(),
-  name: varchar().notNull(),
-  slug: varchar().unique(),
-  email: varchar().notNull(),
-  status: varchar("status").notNull().default("unpublished"),
-  avatarUrl: varchar("avatar_url").notNull().default(""),
-  title: varchar().notNull(),
-  bio: varchar().notNull(),
-  links: jsonb().$type<SocialLink[]>().notNull(),
-});
 export const developerProfiles = pgTable("developer_profiles", {
   id: uuid()
     .primaryKey()
