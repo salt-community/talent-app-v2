fa2fb40d55e5ab599c21b5d2b088e45f25346698
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-03-13 10:50:02 +0100

    refactor: remove ScrollArea from DeveloperDashboard for simplified layout

--- src/features/admin-dashboard/components/developers-dashboard.tsx
+++ src/features/admin-dashboard/components/developers-dashboard.tsx
@@ -2,7 +2,6 @@
 
 import { DeveloperProfileList } from "./developer-profile-list";
 import { Developer } from "../types";
-import { ScrollArea } from "@/components";
 
 type Props = {
   developers: Developer[];
@@ -10,18 +9,16 @@ type Props = {
 
 export function DeveloperDashboard({ developers }: Props) {
   return (
-    <ScrollArea className="h-[calc(100vh-4rem)]">
-      <div>
-        {developers.map((developer) => (
-          <DeveloperProfileList
-            id={developer.id}
-            key={developer.id}
-            name={developer.name}
-            email={developer.email}
-            status={developer.status}
-          />
-        ))}
-      </div>
-    </ScrollArea>
+    <div>
+      {developers.map((developer) => (
+        <DeveloperProfileList
+          id={developer.id}
+          key={developer.id}
+          name={developer.name}
+          email={developer.email}
+          status={developer.status}
+        />
+      ))}
+    </div>
   );
 }
