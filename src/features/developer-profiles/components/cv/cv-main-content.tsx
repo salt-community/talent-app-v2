e2ccb1373f74b45b92c5b4c4a66770dd2bd26a36
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-03-17 10:24:21 +0100

    refactor(cv): move common logic to a function

--- src/features/developer-profiles/components/cv/cv-main-content.tsx
+++ src/features/developer-profiles/components/cv/cv-main-content.tsx
@@ -1,7 +1,8 @@
 import { Button, H2 } from "@/components";
 import { Plus } from "lucide-react";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { v4 } from "uuid";
+import { ExperienceList } from "./experience-list";
 
 type Props = {
   isEditable: boolean;
@@ -29,6 +30,14 @@ export function CvMainContent({ isEditable, jobs, onChange }: Props) {
     },
   ]);
 
+  const createEmptyExperience = () => ({
+    id: v4(),
+    organization: "",
+    date: "",
+    role: "",
+    description: "",
+  });
+
   return (
     <div className="pb-8">
       <div className="flex items-center justify-between px-2">
@@ -40,18 +49,9 @@ export function CvMainContent({ isEditable, jobs, onChange }: Props) {
             variant="default"
             size="icon"
             className="h-5 w-5 rounded-full"
-            onClick={() => {
-              setEducations((prev) => [
-                ...prev,
-                {
-                  id: uuidv4(),
-                  organization: "",
-                  date: "",
-                  role: "",
-                  description: "",
-                },
-              ]);
-            }}
+            onClick={() =>
+              setEducations((prev) => [...prev, createEmptyExperience()])
+            }
           >
             <Plus />
           </Button>
@@ -59,10 +59,10 @@ export function CvMainContent({ isEditable, jobs, onChange }: Props) {
       </div>
       <div className="flex flex-col gap-2 px-2">
         <ExperienceList
-            isEditable={isEditable}
+          isEditable={isEditable}
           experiences={educations}
           onChange={setEducations}
-          />
+        />
       </div>
       <div className="flex justify-between items-center px-2">
         <H2 textColor="text-paragraph px-1 py-1 border border-transparent">
@@ -73,19 +73,7 @@ export function CvMainContent({ isEditable, jobs, onChange }: Props) {
             variant="default"
             size="icon"
             className="h-5 w-5 rounded-full"
-            onClick={() => {
-              const newJobs = [
-                ...jobs,
-                {
-                  id: uuidv4(),
-                  organization: "",
-                  date: "",
-                  role: "",
-                  description: "",
-                },
-              ];
-              onChange(newJobs);
-            }}
+            onClick={() => onChange([...jobs, createEmptyExperience()])}
           >
             <Plus />
           </Button>
@@ -93,10 +81,10 @@ export function CvMainContent({ isEditable, jobs, onChange }: Props) {
       </div>
       <div className="flex flex-col gap-4 px-2 ">
         <ExperienceList
-            isEditable={isEditable}
+          isEditable={isEditable}
           experiences={jobs}
           onChange={onChange}
-          />
+        />
       </div>
     </div>
   );
