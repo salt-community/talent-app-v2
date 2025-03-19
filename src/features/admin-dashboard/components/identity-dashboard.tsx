a44edcb77ee0114e22f3841982a838f514434f56
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-19 10:35:25 +0100

    added search to identity in admin dashboard

--- src/features/admin-dashboard/components/identity-dashboard.tsx
+++ src/features/admin-dashboard/components/identity-dashboard.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import IdentityProfileList from "./identity-profile-list";
 import { Identity } from "../types";
 import IdentityFilter from "./identity-filter";
+import { Input } from "@/components";
 
 type Props = {
   identities: Identity[];
@@ -14,7 +15,11 @@ export function IdentityDashboard({ identities }: Props) {
     core: true,
     admin: true,
   });
+  const [searchFilter, setSearchFilter] = useState("");
 
+  function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+    setSearchFilter(event.target.value);
+  }
   function filterDevelopers(identities: Identity[]) {
     const identitiesFilteredByRole = identities.filter((identity) => {
       if (filterStatus.developer && identity.role === "developer") return true;
@@ -22,31 +27,37 @@ export function IdentityDashboard({ identities }: Props) {
       if (filterStatus.admin && identity.role === "admin") return true;
       return false;
     });
-    // return developersFilteredByStatus.filter((developer) =>
-    //   developer.name.toLocaleLowerCase().includes(searchFilter)
-    // );
-    return identitiesFilteredByRole;
+    const search = identitiesFilteredByRole.filter((identity) =>
+      identity.name.toLocaleLowerCase().includes(searchFilter)
+    );
+    // console.log(search);
+    return search;
   }
   const filteredDevelopers = filterDevelopers(identities);
+  console.log(searchFilter);
 
   return (
     <div>
       <div className="flex justify-between">
+        <div className="w-full bg-white pt-4 pr-4 z-10">
+          <Input placeholder="Type to search" onChange={handleSearchChange} />
+        </div>
         <IdentityFilter
           filterStatus={filterStatus}
           setFilterStatus={setFilterStatus}
         />
       </div>
       <div className="flex flex-col gap-2">
-        {filteredDevelopers.map((identity) => (
-          <IdentityProfileList
-            id={identity.id}
-            key={identity.id}
-            name={identity.name}
-            email={identity.email}
-            role={identity.role}
-          />
-        ))}
+        {filteredDevelopers.length > 0 &&
+          filteredDevelopers.map((identity) => (
+            <IdentityProfileList
+              id={identity.id}
+              key={identity.id}
+              name={identity.name}
+              email={identity.email}
+              role={identity.role}
+            />
+          ))}
       </div>
     </div>
   );
