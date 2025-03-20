6564fa8f889b26da7f125f78a941e88b633e3771
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-20 14:19:26 +0100

    made a new component for the tab links

--- src/app/admin-dashboard/layout.tsx
+++ src/app/admin-dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { Database, Search, UserCog, UserRound } from "lucide-react";
 import Loading from "./loading";
 import Link from "next/link";
+import TabLink from "@/features/admin-dashboard/components/tab-link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -16,50 +17,21 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </h1>
 
       <div className="flex overflow-x-auto border-b mb-4 pb-1">
-        <Link
-          href="/admin-dashboard/developers"
-          className={`px-3 sm:px-4 py-2 flex items-center gap-1 sm:gap-2 text-sm md:text-base whitespace-nowrap cursor-pointer hover:bg-gray-50 ${
-            isActive("/admin-dashboard/developers")
-              ? "border-b-2 border-zinc-400"
-              : ""
-          }`}
-        >
+        <TabLink name="Developer" href="/admin-dashboard/developers">
           <UserRound className="w-5 h-5" />
-          <p>Developers</p>
-        </Link>
-        <Link
-          href="/admin-dashboard/identities"
-          className={`px-3 sm:px-4 py-2 flex items-center gap-1 sm:gap-2 text-sm md:text-base whitespace-nowrap cursor-pointer hover:bg-gray-50 ${
-            isActive("/admin-dashboard/identities")
-              ? "border-b-2 border-zinc-400"
-              : ""
-          }`}
-        >
+        </TabLink>
+        <TabLink name="Identities" href="/admin-dashboard/identities">
           <UserCog className="w-5 h-5" />
-          <p>Identities</p>
-        </Link>
-        <Link
+        </TabLink>
+        <TabLink
+          name="Search"
           href="/admin-dashboard/meilisearch-configuration"
-          className={`px-3 sm:px-4 py-2 flex items-center gap-1 sm:gap-2 text-sm md:text-base whitespace-nowrap cursor-pointer hover:bg-gray-50 ${
-            isActive("/admin-dashboard/meilisearch-configuration")
-              ? "border-b-2 border-zinc-400"
-              : ""
-          }`}
         >
           <Search className="w-5 h-5" />
-          <p>Search</p>
-        </Link>
-        <Link
-          href="/admin-dashboard/database"
-          className={`px-3 sm:px-4 py-2 flex items-center gap-1 sm:gap-2 text-sm md:text-base whitespace-nowrap cursor-pointer hover:bg-gray-50 ${
-            isActive("/admin-dashboard/database")
-              ? "border-b-2 border-zinc-400"
-              : ""
-          }`}
-        >
+        </TabLink>
+        <TabLink name="Database" href="/admin-dashboard/database">
           <Database className="w-5 h-5" />
-          <p>Database</p>
-        </Link>
+        </TabLink>
       </div>
 
       <Suspense fallback={<Loading />}>{children}</Suspense>
