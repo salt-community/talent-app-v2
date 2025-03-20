9cf7915259e25bb3b358967944b78d82a8430b42
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-03-20 17:02:50 +0100

    refactor(developer-profiles): enable CV and remove unused code

--- src/app/developers/[slug]/page.tsx
+++ src/app/developers/[slug]/page.tsx
@@ -1,16 +1,8 @@
-import { DeveloperCard } from "@/components/ui/developer-card";
-// import { ScoreBoard } from "@/features/assignments";
-import {
-  BackgroundCard,
-  developerProfilesService,
-} from "@/features/developer-profiles";
+import { developerProfilesService } from "@/features/developer-profiles";
 import { CV } from "@/features/developer-profiles/components/cv/cv";
 import { iamService } from "@/features/iam";
-import { Projects } from "@/features/projects";
-import { notFound } from "next/navigation";
 
-// Maximum timeout duration for the page speed API
-export const maxDuration = 60;
+import { notFound } from "next/navigation";
 
 type Params = {
   params: Promise<{ slug: string }>;
@@ -23,34 +15,17 @@ export default async function DeveloperDetailPage({ params }: Params) {
   const developerProfile = developerProfiles.find(
     (profile) => profile.slug === slug,
   );
-  if (!developerProfile) {
-    return notFound();
-  }
+
+  if (!developerProfile) return notFound();
 
   const hasProfileAccess = await iamService.hasProfileAccess(
     developerProfile.identityId,
   );
-  // const hasUserAccess =
-  //   await iamService.hasCurrentUserAccess("assignment.score");
 
-  return process.env.FF_DEVELOPER_CV === "ON" ? (
+  return (
     <CV
       developerProfileId={developerProfile.id}
       hasProfileAccess={hasProfileAccess}
     />
-  ) : (
-    <DeveloperCard>
-      <BackgroundCard
-        developerProfileId={developerProfile.id}
-        hasProfileAccess={hasProfileAccess}
-      />
-      {/* {(hasProfileAccess || hasUserAccess) && (
-        <ScoreBoard identityId={developerProfile.identityId} />
-      )} */}
-      <Projects
-        developerProfileId={developerProfile.id}
-        hasProfileAccess={hasProfileAccess}
-      />
-    </DeveloperCard>
   );
 }
