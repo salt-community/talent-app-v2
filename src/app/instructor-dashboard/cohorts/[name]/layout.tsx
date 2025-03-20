4b5287a107f891a4254e2807798b7e10e0271530
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-20 14:47:55 +0100

    updated tab links

--- src/app/instructor-dashboard/cohorts/[name]/layout.tsx
+++ src/app/instructor-dashboard/cohorts/[name]/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React, { Suspense } from "react";
 import { usePathname } from "next/navigation";
-import { Star, Users } from "lucide-react";
-import Loading from "@/app/loading";
+import { Star } from "lucide-react";
+import TabLink from "@/features/admin-dashboard/components/tab-link";
+import Loading from "@/app/admin-dashboard/loading";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -19,19 +20,6 @@ export default function InstructorDashboardLayout({
 
   const normalizePath = (path: string) => path.replace(/\/$/, "");
 
-  const tabs = [
-    {
-      name: "Assignments",
-      icon: <Star className="w-5 h-5" />,
-      href: `/instructor-dashboard/cohorts/${name}`,
-    },
-    {
-      name: "Developers",
-      icon: <Users className="w-5 h-5" />,
-      href: `/instructor-dashboard/cohorts/${name}/developers`,
-    },
-  ];
-
   const getActiveTab = () => {
     const normalizedPath = normalizePath(pathname);
     if (normalizedPath.endsWith("/developers")) return "Developers";
@@ -40,8 +28,6 @@ export default function InstructorDashboardLayout({
     return "";
   };
 
-  const activeTab = getActiveTab();
-
   return (
     <div className="w-full p-3 sm:p-4 md:px-8 lg:px-16 xl:px-32">
       <h1 className="text-xl sm:text-2xl font-bold mb-2 sm:mb-4">
@@ -49,18 +35,18 @@ export default function InstructorDashboardLayout({
       </h1>
 
       <div className="flex overflow-x-auto border-b mb-4 pb-1">
-        {tabs.map((tab) => (
-          <a
-            key={tab.name}
-            href={tab.href}
-            className={`px-3 sm:px-4 py-2 flex items-center gap-1 sm:gap-2 text-sm md:text-base whitespace-nowrap cursor-pointer hover:bg-gray-50 ${
-              activeTab === tab.name ? "border-b-2 border-zinc-400" : ""
-            }`}
-          >
-            {tab.icon}
-            <span>{tab.name}</span>
-          </a>
-        ))}
+        <TabLink
+          name="Assignments"
+          href={`/instructor-dashboard/cohorts/${name}`}
+        >
+          <Star className="w-5 h-5" />
+        </TabLink>
+        <TabLink
+          name="Developers"
+          href={`/instructor-dashboard/cohorts/${name}/developers`}
+        >
+          <Star className="w-5 h-5" />
+        </TabLink>
       </div>
 
       <Suspense fallback={<Loading />}>{children}</Suspense>
