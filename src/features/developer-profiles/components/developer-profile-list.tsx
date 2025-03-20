a2a1539e5ed158ee9bf5c6255bd077d72d1c6339
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-03-20 12:59:54 +0100

    fix(developer-profiles): add key prop to DeveloperProfileCard for list rendering

--- src/features/developer-profiles/components/developer-profile-list.tsx
+++ src/features/developer-profiles/components/developer-profile-list.tsx
@@ -18,7 +18,7 @@ export async function DeveloperProfileList() {
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
         {Array.isArray(profiles.developerProfile) &&
           profiles.developerProfile.map((profile) => (
-            <DeveloperProfileCard developerProfileId={profile.id} />
+            <DeveloperProfileCard developerProfileId={profile.id} key={profile.id}/>
           ))}
       </div>
       <div className="mb-6">
