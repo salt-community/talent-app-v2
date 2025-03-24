91d72d2b0f904511a965ba04b013ef47e53c288f
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-03-24 10:47:08 +0100

    refactor(admin-dashboard): extract tab links into a separate tabs array

--- src/app/admin-dashboard/layout.tsx
+++ src/app/admin-dashboard/layout.tsx
@@ -11,24 +11,39 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </h1>
 
       <div className="flex overflow-x-auto border-b mb-4 pb-1">
-        <TabLink name="Developer" href="/admin-dashboard/developers">
-          <UserRound className="w-5 h-5" />
-        </TabLink>
-        <TabLink name="Identities" href="/admin-dashboard/identities">
-          <UserCog className="w-5 h-5" />
-        </TabLink>
-        <TabLink
-          name="Search"
-          href="/admin-dashboard/meilisearch-configuration"
-        >
-          <Search className="w-5 h-5" />
-        </TabLink>
-        <TabLink name="Database" href="/admin-dashboard/database">
-          <Database className="w-5 h-5" />
-        </TabLink>
+        {tabs.map((tab) => (
+          <TabLink key={tab.name} name={tab.name} href={tab.href}>
+            {tab.icon}
+          </TabLink>
+        ))}
       </div>
 
       <Suspense fallback={<Loading />}>{children}</Suspense>
     </div>
   );
 }
+
+const iconStyle = "w-5 h-5";
+
+const tabs = [
+  {
+    name: "Developer",
+    href: "/admin-dashboard/developers",
+    icon: <UserRound className={iconStyle} />,
+  },
+  {
+    name: "Identities",
+    href: "/admin-dashboard/identities",
+    icon: <UserCog className={iconStyle} />,
+  },
+  {
+    name: "Search",
+    href: "/admin-dashboard/meilisearch-configuration",
+    icon: <Search className={iconStyle} />,
+  },
+  {
+    name: "Database",
+    href: "/admin-dashboard/database",
+    icon: <Database className={iconStyle} />,
+  },
+];
