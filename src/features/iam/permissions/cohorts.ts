9aadbbdeeaec1fc5e82d004d94c4d8f1126a1631
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-04-24 11:39:09 +0200

    refactor: remove unused cohort permissions and reorder existing ones

--- src/features/iam/permissions/cohorts.ts
+++ src/features/iam/permissions/cohorts.ts
@@ -3,14 +3,9 @@ import { PermissionsSchema } from "../secure-service";
 
 export const cohorts: PermissionsSchema<"cohorts", CohortsService> = {
   addCohort: "cohorts.addCohort",
-  getCohortById: "cohorts.getCohortById",
-  getAll: "cohorts.getAll",
-  updateCohortStatus: "cohorts.updateCohortStatus",
   getCohortStudents: "cohorts.getCohortStudents",
-  getAllUnassignedDevelopers: "cohorts.getAllUnassignedDevelopers",
-  addDeveloperToCohort: "cohorts.addDeveloperToCohort",
   addDevelopersToCohort: "cohorts.addDevelopersToCohort",
   deleteCohortAndCohortIdentity: "cohorts.deleteCohortAndCohortIdentity",
-  getCohortIdByIdentityId: "cohorts.getCohortIdByIdentityId",
   deleteIdentityFromCohort: "cohorts.deleteIdentityFromCohort",
+  getAll: "cohorts.getAll"
 };
