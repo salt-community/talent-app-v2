f19235e441307d7c21394167d610835201ccad3a
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-04-24 11:24:55 +0200

    refactor: remove unused validation.ts file and its associated schema

--- src/features/assignments/validation.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const assignmentUpdates = z.object({
-  developerProfileId: z.string(), // ev byta till uuid
-  title: z.string().min(1, "Title is required"),
-  score: z.string().min(1, "Score is required"),
-  comment: z.string().optional(),
-  tags: z.array(z.string()).optional(),
-});
