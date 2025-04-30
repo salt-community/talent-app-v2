75578edf932b456f7d12509f499a36ce0ce27887
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-04-30 18:48:22 +0200

    refactor: restructure Page component to include header and description, enhancing layout clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
 import { HighlightPage } from "@/features/developer-profiles";
+import { Suspense } from "react";
 
-
-export default HighlightPage;
+export default function Page() {
+  return (
+    <div>
+      <h2 className="text-center text-5xl font-extrabold pt-16 text-header px-1">
+        Our Developers in Focus
+      </h2>
+      <h3 className="text-center text-lg pt-6 text-paragraphLight px-6">
+        Each developer has a unique story, skill set, and impact on the tech
+        world. Meet a standout talent and explore their journey.
+      </h3>
+      <HighlightPage />
+    </div>
+  );
+}
--- src/features/developer-profiles/components/highlight-page.tsx
+++ src/features/developer-profiles/components/highlight-page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { developerProfilesService } from "../instance";
 import { HighlightedDevelopers } from "./highlighted-developers";
 
@@ -6,19 +7,8 @@ export async function HighlightPage() {
     await developerProfilesService.getHighlightedDeveloperProfiles();
 
   return (
-    <div>
-      <h2 className="text-center text-5xl font-extrabold pt-16 text-header px-1">
-        Our Developers in Focus
-      </h2>
-      <h3 className="text-center text-lg pt-6 text-paragraphLight px-6">
-        Each developer has a unique story, skill set, and impact on the tech
-        world. Meet a standout talent and explore their journey.
-      </h3>
-      <div className="md:px-4 pb-12">
-        <HighlightedDevelopers
-          developerProfiles={highlightedDeveloperProfiles}
-        />
-      </div>
+    <div className="md:px-4 pb-12">
+      <HighlightedDevelopers developerProfiles={highlightedDeveloperProfiles} />
     </div>
   );
 }
