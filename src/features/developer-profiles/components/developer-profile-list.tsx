72e061f9f504f215d437a65c30341573100b4797
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-05-01 17:30:18 +0200

    refactor: update layout of developer profile list for improved responsiveness and visual structure

--- src/features/developer-profiles/components/developer-profile-list.tsx
+++ src/features/developer-profiles/components/developer-profile-list.tsx
@@ -12,14 +12,16 @@ export async function DeveloperProfileList() {
   }
 
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-      {Array.isArray(profiles.developerProfile) &&
-        profiles.developerProfile.map((profile) => (
+    <div className="mx-auto md:px-0 py-8 flex flex-col gap-6">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        {Array.isArray(profiles.developerProfile) &&
+          profiles.developerProfile.map((profile) => (
           <DeveloperProfileCard
             developerProfileId={profile.id}
             key={profile.id}
           />
         ))}
+      </div>
       <div className="mb-6">
         <CreateProfileButton identityId={identityId} />
       </div>
