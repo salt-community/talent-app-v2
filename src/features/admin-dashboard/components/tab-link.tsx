2e1bf47e4f5525fad2b29825ddc654b473fc5700
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-05-01 20:18:47 +0200

    refactor: enable prefetching for TabLink component to improve navigation performance

--- src/features/admin-dashboard/components/tab-link.tsx
+++ src/features/admin-dashboard/components/tab-link.tsx
@@ -14,6 +14,7 @@ export default function TabLink({ name, href, children }: Props) {
   return (
     <Link
       href={href}
+      prefetch={true}
       className={`px-3 sm:px-4 py-2 flex items-center gap-1 sm:gap-2 text-sm md:text-base whitespace-nowrap cursor-pointer hover:bg-gray-50 ${
         isActive(href) ? "border-b-2 border-zinc-400" : ""
       }`}
